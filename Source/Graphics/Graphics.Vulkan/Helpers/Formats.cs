171ca11f0f56e2a6ed56ab237f5221047f9b937b
王先生 <15718810177@163.com>
2024-07-01 19:14:54 +0800

    增强Vulkan纹理和格式支持
    在`Texture.cs`中，引入了`Graphics.Core;`和`Silk.NET.Vulkan;`命名空间，并对`Texture`类增加了新的构造函数代码块，用于初始化`ImageCreateInfo`结构体。这一步骤包括设置图像类型、尺寸、初始布局、使用标志、平铺方式、格式、创建标志以及样本数量。特别地，如果`TextureUsage`中包含`Cubemap`，则会额外添加`CreateCubeCompatibleBit`标志。
    
    在`Formats.cs`文件中，新增了一个`Formats`静态类，提供了几个静态方法来将图形库中的枚举类型转换为Vulkan特定的枚举值。这包括：
    - `GetPixelFormat`：将`PixelFormat`和深度格式标志转换为Vulkan `Format`。
    - `GetImageType`：将`TextureType`转换为Vulkan `ImageType`。
    - `GetImageUsageFlags`：将`TextureUsage`转换为Vulkan `ImageUsageFlags`。
    - `GetSampleCount`：将`TextureSampleCount`转换为Vulkan `SampleCountFlags`。
    
    这些更改旨在提高Vulkan图形API中对不同纹理格式、图像类型、使用方式和采样计数的支持，同时也提高了代码的模块化和易于管理性。

---
+++ Source/Graphics/Graphics.Vulkan/Helpers/Formats.cs
@@ -0,0 +1,141 @@
+﻿using Graphics.Core;
+using Silk.NET.Vulkan;
+
+namespace Graphics.Vulkan;
+
+internal static class Formats
+{
+    public static Format GetPixelFormat(PixelFormat pixelFormat, bool depthFormat)
+    {
+        return pixelFormat switch
+        {
+            PixelFormat.R8UNorm => Format.R8Unorm,
+            PixelFormat.R8SNorm => Format.R8SNorm,
+            PixelFormat.R8UInt => Format.R8Uint,
+            PixelFormat.R8SInt => Format.R8Sint,
+
+            PixelFormat.R16UNorm => depthFormat ? Format.D16Unorm : Format.R16Unorm,
+            PixelFormat.R16SNorm => Format.R16SNorm,
+            PixelFormat.R16UInt => Format.R16Uint,
+            PixelFormat.R16SInt => Format.R16Sint,
+            PixelFormat.R16Float => Format.R16Sfloat,
+
+            PixelFormat.R32UInt => Format.R32Uint,
+            PixelFormat.R32SInt => Format.R32Sint,
+            PixelFormat.R32Float => depthFormat ? Format.D32Sfloat : Format.R32Sfloat,
+
+            PixelFormat.R8G8UNorm => Format.R8G8Unorm,
+            PixelFormat.R8G8SNorm => Format.R8G8SNorm,
+            PixelFormat.R8G8UInt => Format.R8G8Uint,
+            PixelFormat.R8G8SInt => Format.R8G8Sint,
+
+            PixelFormat.R16G16UNorm => Format.R16G16Unorm,
+            PixelFormat.R16G16SNorm => Format.R16G16SNorm,
+            PixelFormat.R16G16UInt => Format.R16G16Uint,
+            PixelFormat.R16G16SInt => Format.R16G16Sint,
+            PixelFormat.R16G16Float => Format.R16G16Sfloat,
+
+            PixelFormat.R32G32UInt => Format.R32G32Uint,
+            PixelFormat.R32G32SInt => Format.R32G32Sint,
+            PixelFormat.R32G32Float => Format.R32G32Sfloat,
+
+            PixelFormat.R8G8B8A8UNorm => Format.R8G8B8A8Unorm,
+            PixelFormat.R8G8B8A8UNormSRgb => Format.R8G8B8A8Srgb,
+            PixelFormat.B8G8R8A8UNorm => Format.B8G8R8A8Unorm,
+            PixelFormat.B8G8R8A8UNormSRgb => Format.B8G8R8A8Srgb,
+            PixelFormat.R8G8B8A8SNorm => Format.R8G8B8A8SNorm,
+            PixelFormat.R8G8B8A8UInt => Format.R8G8B8A8Uint,
+            PixelFormat.R8G8B8A8SInt => Format.R8G8B8A8Sint,
+
+            PixelFormat.R16G16B16A16UNorm => Format.R16G16B16A16Unorm,
+            PixelFormat.R16G16B16A16SNorm => Format.R16G16B16A16SNorm,
+            PixelFormat.R16G16B16A16UInt => Format.R16G16B16A16Uint,
+            PixelFormat.R16G16B16A16SInt => Format.R16G16B16A16Sint,
+            PixelFormat.R16G16B16A16Float => Format.R16G16B16A16Sfloat,
+
+            PixelFormat.R32G32B32A32UInt => Format.R32G32B32A32Uint,
+            PixelFormat.R32G32B32A32SInt => Format.R32G32B32A32Sint,
+            PixelFormat.R32G32B32A32Float => Format.R32G32B32A32Sfloat,
+
+            PixelFormat.BC1RgbUNorm => Format.BC1RgbUnormBlock,
+            PixelFormat.BC1RgbUNormSRgb => Format.BC1RgbSrgbBlock,
+            PixelFormat.BC1RgbaUNorm => Format.BC1RgbaUnormBlock,
+            PixelFormat.BC1RgbaUNormSRgb => Format.BC1RgbaSrgbBlock,
+            PixelFormat.BC2UNorm => Format.BC2UnormBlock,
+            PixelFormat.BC2UNormSRgb => Format.BC2SrgbBlock,
+            PixelFormat.BC3UNorm => Format.BC3UnormBlock,
+            PixelFormat.BC3UNormSRgb => Format.BC3SrgbBlock,
+            PixelFormat.BC4UNorm => Format.BC4UnormBlock,
+            PixelFormat.BC4SNorm => Format.BC4SNormBlock,
+            PixelFormat.BC5UNorm => Format.BC5UnormBlock,
+            PixelFormat.BC5SNorm => Format.BC5SNormBlock,
+            PixelFormat.BC7UNorm => Format.BC7UnormBlock,
+            PixelFormat.BC7UNormSRgb => Format.BC7SrgbBlock,
+
+            PixelFormat.ETC2R8G8B8UNorm => Format.Etc2R8G8B8UnormBlock,
+            PixelFormat.ETC2R8G8B8A1UNorm => Format.Etc2R8G8B8A1UnormBlock,
+            PixelFormat.ETC2R8G8B8A8UNorm => Format.Etc2R8G8B8A8UnormBlock,
+
+            PixelFormat.D32FloatS8UInt => Format.D32SfloatS8Uint,
+            PixelFormat.D24UNormS8UInt => Format.D24UnormS8Uint,
+
+            PixelFormat.R10G10B10A2UNorm => Format.A2B10G10R10UnormPack32,
+            PixelFormat.R10G10B10A2UInt => Format.A2B10G10R10UintPack32,
+            PixelFormat.R11G11B10Float => Format.B10G11R11UfloatPack32,
+
+            _ => throw new ArgumentOutOfRangeException(nameof(pixelFormat))
+        };
+    }
+
+    public static ImageType GetImageType(TextureType textureType)
+    {
+        return textureType switch
+        {
+            TextureType.Texture1D => ImageType.Type1D,
+            TextureType.Texture2D => ImageType.Type2D,
+            TextureType.Texture3D => ImageType.Type3D,
+            _ => throw new ArgumentOutOfRangeException(nameof(textureType))
+        };
+    }
+
+    public static ImageUsageFlags GetImageUsageFlags(TextureUsage textureUsage)
+    {
+        ImageUsageFlags imageUsageFlags = ImageUsageFlags.TransferDstBit | ImageUsageFlags.TransferSrcBit;
+
+        if (textureUsage.HasFlag(TextureUsage.RenderTarget))
+        {
+            imageUsageFlags |= ImageUsageFlags.ColorAttachmentBit;
+        }
+
+        if (textureUsage.HasFlag(TextureUsage.DepthStencil))
+        {
+            imageUsageFlags |= ImageUsageFlags.DepthStencilAttachmentBit;
+        }
+
+        if (textureUsage.HasFlag(TextureUsage.Sampled))
+        {
+            imageUsageFlags |= ImageUsageFlags.SampledBit;
+        }
+
+        if (textureUsage.HasFlag(TextureUsage.Storage))
+        {
+            imageUsageFlags |= ImageUsageFlags.StorageBit;
+        }
+
+        return imageUsageFlags;
+    }
+
+    public static SampleCountFlags GetSampleCount(TextureSampleCount sampleCount)
+    {
+        return sampleCount switch
+        {
+            TextureSampleCount.Count1 => SampleCountFlags.Count1Bit,
+            TextureSampleCount.Count2 => SampleCountFlags.Count2Bit,
+            TextureSampleCount.Count4 => SampleCountFlags.Count4Bit,
+            TextureSampleCount.Count8 => SampleCountFlags.Count8Bit,
+            TextureSampleCount.Count16 => SampleCountFlags.Count16Bit,
+            TextureSampleCount.Count32 => SampleCountFlags.Count32Bit,
+            _ => throw new ArgumentOutOfRangeException(nameof(sampleCount))
+        };
+    }
+}
--- Source/Graphics/Graphics.Vulkan/Resources/Texture.cs
+++ Source/Graphics/Graphics.Vulkan/Resources/Texture.cs
@@ -1,9 +1,34 @@
-﻿namespace Graphics.Vulkan;
+﻿using Graphics.Core;
+using Silk.NET.Vulkan;
+
+namespace Graphics.Vulkan;
 
 public class Texture : DeviceResource
 {
     internal Texture(GraphicsDevice graphicsDevice, in TextureDescription description) : base(graphicsDevice)
     {
+        ImageCreateInfo imageCreateInfo = new()
+        {
+            SType = StructureType.ImageCreateInfo,
+            ImageType = Formats.GetImageType(description.Type),
+            Extent = new Extent3D
+            {
+                Width = description.Width,
+                Height = description.Height,
+                Depth = description.Depth
+            },
+            InitialLayout = ImageLayout.Preinitialized,
+            Usage = Formats.GetImageUsageFlags(description.Usage),
+            Tiling = description.Usage.HasFlag(TextureUsage.Staging) ? ImageTiling.Linear : ImageTiling.Optimal,
+            Format = Formats.GetPixelFormat(description.Format, description.Usage.HasFlag(TextureUsage.DepthStencil)),
+            Flags = ImageCreateFlags.CreateMutableFormatBit,
+            Samples = Formats.GetSampleCount(description.SampleCount)
+        };
+
+        if (description.Usage.HasFlag(TextureUsage.Cubemap))
+        {
+            imageCreateInfo.Flags |= ImageCreateFlags.CreateCubeCompatibleBit;
+        }
     }
 
     protected override void Destroy()
