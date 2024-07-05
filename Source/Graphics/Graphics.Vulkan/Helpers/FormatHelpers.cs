8aa243b7e1e80712a8f182e8a8decded716c39d8
王先生 <15718810177@163.com>
2024-07-05 19:39:50 +0800

    更新Framebuffer和新增FormatHelpers类
    在 `Framebuffer.cs` 中，我们进行了一系列重要的更改，包括移除旧的命名空间并引入新的命名空间，将 `Framebuffer` 类标记为 `unsafe` 并使其继承自 `DeviceResource`。此外，我们还添加了一个私有只读字段 `_renderPass`，并在构造函数中实现了渲染通道的创建逻辑，这包括颜色附件和深度附件的描述、引用以及子通道的描述和依赖关系的设置。在 `Destroy` 方法中，我们添加了渲染通道的销毁逻辑。
    
    对于 `FormatHelpers.cs` 文件，我们新增了一个 `FormatHelpers` 类，该类提供了格式帮助方法。其中包括一个新的静态方法 `IsStencilFormat`，用于判断给定的像素格式是否为模板格式，目前支持的模板格式有 `PixelFormat.D24UNormS8UInt` 和 `PixelFormat.D32FloatS8UInt`。
    
    这些更改旨在提高代码的可维护性和扩展性，同时为未来的开发工作奠定基础。

---
+++ Source/Graphics/Graphics.Vulkan/Helpers/FormatHelpers.cs
@@ -0,0 +1,17 @@
+﻿using Graphics.Core;
+
+namespace Graphics.Vulkan;
+
+internal static class FormatHelpers
+{
+    public static bool IsStencilFormat(PixelFormat format)
+    {
+        return format switch
+        {
+            PixelFormat.D24UNormS8UInt or
+            PixelFormat.D32FloatS8UInt => true,
+
+            _ => false
+        };
+    }
+}
--- Source/Graphics/Graphics.Vulkan/Resources/Framebuffer.cs
+++ Source/Graphics/Graphics.Vulkan/Resources/Framebuffer.cs
@@ -1,12 +1,125 @@
-﻿namespace Graphics.Vulkan;
+﻿using System.Runtime.CompilerServices;
+using Graphics.Core;
+using Silk.NET.Vulkan;
 
-public class Framebuffer : DeviceResource
+namespace Graphics.Vulkan;
+
+public unsafe class Framebuffer : DeviceResource
 {
+    private readonly RenderPass _renderPass;
+
     public Framebuffer(GraphicsDevice graphicsDevice, ref readonly FramebufferDescription description, bool isPresented) : base(graphicsDevice)
     {
+        bool hasDepth = description.DepthTarget.HasValue;
+
+        uint colorAttachmentCount = (uint)description.ColorTargets.Length;
+        uint depthAttachmentCount = hasDepth ? 1u : 0u;
+        uint attachmentCount = colorAttachmentCount + depthAttachmentCount;
+
+        AttachmentDescription[] attachments = new AttachmentDescription[attachmentCount];
+        AttachmentReference[] references = new AttachmentReference[attachmentCount];
+
+        for (uint i = 0; i < description.ColorTargets.Length; i++)
+        {
+            Texture colorTarget = description.ColorTargets[i].Target;
+
+            ImageLayout initialLayout = isPresented
+                ? ImageLayout.PresentSrcKhr
+                : colorTarget.Usage.HasFlag(TextureUsage.Sampled) ? ImageLayout.ShaderReadOnlyOptimal : ImageLayout.ColorAttachmentOptimal;
+
+            attachments[i] = new AttachmentDescription
+            {
+                Format = colorTarget.VkFormat,
+                Samples = colorTarget.VkSampleCount,
+                LoadOp = AttachmentLoadOp.Clear,
+                StoreOp = AttachmentStoreOp.Store,
+                StencilLoadOp = AttachmentLoadOp.DontCare,
+                StencilStoreOp = AttachmentStoreOp.DontCare,
+                InitialLayout = initialLayout,
+                FinalLayout = ImageLayout.ColorAttachmentOptimal
+            };
+
+            references[i] = new AttachmentReference
+            {
+                Attachment = i,
+                Layout = ImageLayout.ColorAttachmentOptimal
+            };
+        }
+
+        if (hasDepth)
+        {
+            Texture depthTarget = description.DepthTarget!.Value.Target;
+
+            bool hasStencil = FormatHelpers.IsStencilFormat(depthTarget.Format);
+
+            ImageLayout initialLayout = depthTarget.Usage.HasFlag(TextureUsage.Sampled)
+                ? ImageLayout.DepthStencilReadOnlyOptimal
+                : ImageLayout.DepthStencilAttachmentOptimal;
+
+            attachments[^1] = new AttachmentDescription
+            {
+                Format = depthTarget.VkFormat,
+                Samples = depthTarget.VkSampleCount,
+                LoadOp = AttachmentLoadOp.Load,
+                StoreOp = AttachmentStoreOp.Store,
+                StencilLoadOp = AttachmentLoadOp.DontCare,
+                StencilStoreOp = hasStencil ? AttachmentStoreOp.Store : AttachmentStoreOp.DontCare,
+                InitialLayout = initialLayout,
+                FinalLayout = ImageLayout.DepthStencilAttachmentOptimal
+            };
+
+            references[^1] = new AttachmentReference
+            {
+                Attachment = attachmentCount - 1,
+                Layout = ImageLayout.DepthStencilAttachmentOptimal
+            };
+        }
+
+        SubpassDescription subpass = new()
+        {
+            PipelineBindPoint = PipelineBindPoint.Graphics
+        };
+
+        if (colorAttachmentCount > 0)
+        {
+            subpass.ColorAttachmentCount = colorAttachmentCount;
+            subpass.PColorAttachments = (AttachmentReference*)Unsafe.AsPointer(ref references[0]);
+        }
+
+        if (hasDepth)
+        {
+            subpass.PDepthStencilAttachment = (AttachmentReference*)Unsafe.AsPointer(ref references[^1]);
+        }
+
+        SubpassDependency subpassDependency = new()
+        {
+            SrcSubpass = Vk.SubpassExternal,
+            DstSubpass = 0,
+            SrcStageMask = PipelineStageFlags.ColorAttachmentOutputBit,
+            SrcAccessMask = AccessFlags.None,
+            DstStageMask = PipelineStageFlags.ColorAttachmentOutputBit,
+            DstAccessMask = AccessFlags.ColorAttachmentReadBit | AccessFlags.ColorAttachmentWriteBit
+        };
+
+        RenderPassCreateInfo createInfo = new()
+        {
+            SType = StructureType.RenderPassCreateInfo,
+            AttachmentCount = attachmentCount,
+            PAttachments = (AttachmentDescription*)Unsafe.AsPointer(ref attachments[0]),
+            SubpassCount = 1,
+            PSubpasses = &subpass,
+            DependencyCount = 1,
+            PDependencies = &subpassDependency
+        };
+
+        RenderPass renderPass;
+        Vk.CreateRenderPass(graphicsDevice.Device, &createInfo, null, &renderPass).ThrowCode();
+
+        _renderPass = renderPass;
     }
 
     protected override void Destroy()
     {
+        Vk.DestroyRenderPass(GraphicsDevice.Device, _renderPass, null);
     }
 }
