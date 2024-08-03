ea46b5485daedf30488a0d0ea79db816a141fbcf
王先生 <1324771795@qq.com>
2024-08-04 01:21:34 +0800

    增加对 GLTF 节点中网格数据的加载和处理
    在 `GLTFScene.cs` 文件中对 `GLTFScene` 类进行了修改，主要增加了对 GLTF 节点中网格数据的加载和处理。
    在 `LoadNode` 方法中，增加了对 `gltfNode.Mesh` 的检查，如果存在网格数据，则进一步处理其网格原语（`MeshPrimitive`）。
    新增了两个列表：`vertices` 和 `indices`，分别用于存储顶点和索引数据。
    从 `VertexAccessors` 中提取顶点属性，并将这些属性添加到 `vertices` 列表中。
    从 `IndexAccessor` 中提取索引，并将其添加到 `indices` 列表中，同时调整索引值以考虑顶点偏移。
    为当前节点的网格添加一个新的原语，包含起始索引、索引数量和材质索引。

--- Source/Renderer/Scenes/GLTFScene.cs
+++ Source/Renderer/Scenes/GLTFScene.cs
@@ -130,6 +130,9 @@ internal sealed unsafe class GLTFScene(MainWindow mainWindow) : Scene(mainWindow
             _materials.Add(material);
         }
 
+        List<Vertex> vertices = [];
+        List<uint> indices = [];
+
         foreach (GLTFNode gltfNode in root.LogicalNodes)
         {
             LoadNode(gltfNode, null);
@@ -146,6 +149,81 @@ internal sealed unsafe class GLTFScene(MainWindow mainWindow) : Scene(mainWindow
             foreach (GLTFNode children in gltfNode.VisualChildren)
             {
                 LoadNode(children, node);
+            }
+
+            if (gltfNode.Mesh != null)
+            {
+                foreach (MeshPrimitive primitive in gltfNode.Mesh.Primitives)
+                {
+                    uint firsetIndex = (uint)indices.Count;
+                    uint vertexOffset = (uint)vertices.Count;
+                    int indexCount = 0;
+
+                    // Vertices
+                    {
+                        IList<Vector3>? positionBuffer = null;
+                        IList<Vector3>? normalBuffer = null;
+                        IList<Vector2>? texCoordBuffer = null;
+                        IList<Vector3>? colorBuffer = null;
+                        IList<Vector4>? tangentBuffer = null;
+                        uint vertexCount = 0;
+
+                        if (primitive.VertexAccessors.TryGetValue("POSITION", out Accessor? positionAccessor))
+                        {
+                            positionBuffer = positionAccessor.AsVector3Array();
+                            vertexCount = (uint)positionAccessor.Count;
+                        }
+
+                        if (primitive.VertexAccessors.TryGetValue("NORMAL", out Accessor? normalAccessor))
+                        {
+                            normalBuffer = normalAccessor.AsVector3Array();
+                        }
+
+                        if (primitive.VertexAccessors.TryGetValue("TEXCOORD_0", out Accessor? texCoordAccessor))
+                        {
+                            texCoordBuffer = texCoordAccessor.AsVector2Array();
+                        }
+
+                        if (primitive.VertexAccessors.TryGetValue("COLOR_0", out Accessor? colorAccessor))
+                        {
+                            colorBuffer = colorAccessor.AsVector3Array();
+                        }
+
+                        if (primitive.VertexAccessors.TryGetValue("TANGENT", out Accessor? tangentAccessor))
+                        {
+                            tangentBuffer = tangentAccessor.AsVector4Array();
+                        }
+
+                        for (uint i = 0; i < vertexCount; i++)
+                        {
+                            Vector3 position = positionBuffer != null ? positionBuffer[(int)i] : Vector3.Zero;
+                            Vector3 normal = normalBuffer != null ? normalBuffer[(int)i] : Vector3.Zero;
+                            Vector2 texCoord = texCoordBuffer != null ? texCoordBuffer[(int)i] : Vector2.Zero;
+                            Vector3 color = colorBuffer != null ? colorBuffer[(int)i] : Vector3.Zero;
+                            Vector4 tangent = tangentBuffer != null ? tangentBuffer[(int)i] : Vector4.Zero;
+
+                            vertices.Add(new Vertex(position, normal, texCoord, color, tangent));
+                        }
+                    }
+
+                    // Indices
+                    {
+                        if (primitive.IndexAccessor != null)
+                        {
+                            indexCount = primitive.IndexAccessor.Count;
+
+                            IList<uint>? indexBuffer = primitive.IndexAccessor.AsIndicesArray();
+
+                            for (int i = 0; i < indexCount; i++)
+                            {
+                                indices.Add(indexBuffer[i] + vertexOffset);
+                            }
+                        }
+                    }
+
+                    node.Mesh ??= new();
+                    node.Mesh.Primitives.Add(new Primitive(firsetIndex, (uint)indexCount, primitive.Material.LogicalIndex));
+                }
             }
 
             if (parent != null)
