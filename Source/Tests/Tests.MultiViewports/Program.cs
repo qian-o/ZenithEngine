1504e0c6a9d319d29538da3187a2a150faf10f88
王先生 <1324771795@qq.com>
2024-08-07 17:23:47 +0800

    添加 Vulkan 和 ImGui 支持，配置 DPI 感知
    在 `Tests.Core.csproj` 文件中，添加了一个新的 `ItemGroup`，用于将 `Assets` 目录下的所有文件复制到输出目录。
    
    在 `Program.cs` 文件中，进行了以下更改：
    - 添加了对 `Graphics.Vulkan` 和 `Hexa.NET.ImGui` 的引用。
    - 将 `Window` 的创建和配置代码封装在一个新的 `Program` 类中。
    - 添加了 `Context`、`GraphicsDevice`、`CommandList` 和 `ImGuiController` 的初始化和使用代码。
    - 添加了 `Window_Update`、`Window_Render` 和 `Window_Resize` 事件处理方法。
    
    在 `Tests.MultiViewports.csproj` 文件中，添加了一个新的 `PropertyGroup`，指定了应用程序的清单文件 `app.manifest`。
    
    新增了 `app.manifest` 文件，配置了应用程序的 DPI 感知设置。

--- Source/Tests/Tests.MultiViewports/Program.cs
+++ Source/Tests/Tests.MultiViewports/Program.cs
@@ -1,5 +1,56 @@
 ﻿using Graphics.Core;
+using Graphics.Vulkan;
+using Hexa.NET.ImGui;
 
-using Window window = Window.CreateWindowByVulkan();
+internal sealed unsafe class Program
+{
+    private static void Main(string[] _)
+    {
+        using Window window = Window.CreateWindowByVulkan();
+        window.Title = "Tests.MultiViewports";
+        window.MinimumSize = new(100, 100);
 
-window.Run();
+        using Context context = new();
+        using GraphicsDevice device = context.CreateGraphicsDevice(context.EnumeratePhysicalDevices().First(), window);
+        using CommandList commandList = device.ResourceFactory.CreateGraphicsCommandList();
+        using ImGuiController imGuiController = new(window,
+                                                    device,
+                                                    new ImGuiFontConfig("Assets/Fonts/MSYH.TTC", 14, (a) => (nint)a.Fonts.GetGlyphRangesChineseFull()));
+
+        window.Update += Window_Update;
+        window.Render += Window_Render;
+        window.Resize += Window_Resize;
+
+        window.Run();
+
+        void Window_Update(object? sender, UpdateEventArgs e)
+        {
+            imGuiController.Update(e.DeltaTime);
+        }
+
+        void Window_Render(object? sender, RenderEventArgs e)
+        {
+            ImGui.ShowDemoWindow();
+
+            commandList.Begin();
+            {
+                commandList.SetFramebuffer(device.MainSwapchain.Framebuffer);
+                commandList.ClearColorTarget(0, RgbaFloat.Black);
+                commandList.ClearDepthStencil(1.0f);
+
+                imGuiController.Render(commandList);
+            }
+            commandList.End();
+
+            device.SubmitCommands(commandList);
+
+            device.SwapBuffers();
+            imGuiController.PlatformSwapBuffers();
+        }
+
+        void Window_Resize(object? sender, ResizeEventArgs e)
+        {
+            device.MainSwapchain.Resize(e.Width, e.Height);
+        }
+    }
+}
