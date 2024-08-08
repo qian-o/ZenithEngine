bf43d01a6f77363e889b1a13e7509dc9a0a71e53
王先生 <1324771795@qq.com>
2024-08-08 19:10:53 +0800

    添加 GltfSceneRendering 测试项目及 Vulkan 窗口支持
    在 `Renderer.sln` 文件中，添加了 `Tests.GltfSceneRendering` 项目，并更新了相应的项目配置和嵌套项目设置。
    在 `app.manifest` 文件中，添加了一个新的 XML 清单文件，启用了 DPI 感知和每监视器 DPI 感知设置。
    在 `Program.cs` 文件中，添加了一个新的程序入口，使用 `Graphics.Core` 和 `Graphics.Vulkan` 库创建了一个 Vulkan 窗口，并设置了窗口的加载、更新、渲染和关闭事件处理程序。
    添加了 `Tests.GltfSceneRendering.csproj` 项目文件，配置了项目的输出类型、目标框架、隐式使用、可空性、允许不安全代码块、分析级别和代码风格强制。在项目中引用了 `Tests.Core` 项目，并指定了应用程序清单文件。

---
+++ Source/Tests/Tests.GltfSceneRendering/Program.cs
@@ -0,0 +1,44 @@
+﻿using Graphics.Core;
+using Graphics.Vulkan;
+
+internal sealed unsafe class Program
+{
+    private static GraphicsDevice _device = null!;
+
+    private static void Main(string[] _)
+    {
+        using Window window = Window.CreateWindowByVulkan();
+        window.Title = "Tests.MultiViewports";
+        window.MinimumSize = new(100, 100);
+
+        using Context context = new();
+        using GraphicsDevice device = context.CreateGraphicsDevice(context.EnumeratePhysicalDevices().First(), window);
+
+        _device = device;
+
+        window.Load += Window_Load;
+        window.Update += Window_Update;
+        window.Render += Window_Render;
+        window.Resize += (_, e) => _device.MainSwapchain.Resize(e.Width, e.Height);
+        window.Closing += Window_Closing;
+
+        window.Run();
+    }
+
+    private static void Window_Load(object? sender, LoadEventArgs e)
+    {
+
+    }
+
+    private static void Window_Update(object? sender, UpdateEventArgs e)
+    {
+    }
+
+    private static void Window_Render(object? sender, RenderEventArgs e)
+    {
+    }
+
+    private static void Window_Closing(object? sender, ClosingEventArgs e)
+    {
+    }
+}
