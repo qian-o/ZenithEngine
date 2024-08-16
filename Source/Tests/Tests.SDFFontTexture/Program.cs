0d24dcd11fa3aca9070a27715fbea5a50d9b5416
王先生 <1324771795@qq.com>
2024-08-16 16:28:38 +0800

    添加 Tests.SDFFontTexture 项目及 DPI 感知配置
    在 `Renderer.sln` 文件中，添加了一个新的项目 `Tests.SDFFontTexture`，其路径为 `Source\Tests\Tests.SDFFontTexture\Tests.SDFFontTexture.csproj`，并且在解决方案配置中添加了相应的构建配置。
    
    在 `app.manifest` 文件中，添加了一个新的 XML 配置文件，定义了 DPI 感知设置。
    
    在 `Program.cs` 文件中，添加了一个新的程序入口，使用 `Graphics.Core` 和 `Graphics.Vulkan` 库来创建一个窗口，并设置了窗口的标题、最小尺寸、加载、更新、渲染和调整大小的事件处理程序。
    
    在 `Tests.SDFFontTexture.csproj` 文件中，添加了一个新的项目文件，定义了项目的输出类型、目标框架、隐式使用、可空性、允许不安全代码块、分析级别、代码风格强制以及应用程序清单文件。同时，添加了对 `Tests.Core` 项目的引用。

---
+++ Source/Tests/Tests.SDFFontTexture/Program.cs
@@ -0,0 +1,80 @@
+﻿using Graphics.Core;
+using Graphics.Vulkan;
+
+internal sealed unsafe class Program
+{
+    private static GraphicsDevice _device = null!;
+    private static CommandList _mainCommandList = null!;
+
+    private static void Main(string[] _)
+    {
+        using Window window = Window.CreateWindowByVulkan();
+        window.Title = "Tests.SDFFontTexture";
+        window.MinimumSize = new(100, 100);
+
+        using Context context = new();
+        using GraphicsDevice device = context.CreateGraphicsDevice(context.EnumeratePhysicalDevices().First(), window);
+        using CommandList commandList = device.ResourceFactory.CreateGraphicsCommandList();
+        using ImGuiController imGuiController = new(window,
+                                                    device,
+                                                    new ImGuiFontConfig("Assets/Fonts/MSYH.TTC", 14, (a) => (nint)a.Fonts.GetGlyphRangesChineseFull()));
+
+        window.Load += Load;
+
+        window.Update += (a, b) =>
+        {
+            imGuiController.Update(b.DeltaTime);
+
+            Update(a, b);
+        };
+
+        window.Render += (a, b) =>
+        {
+            commandList.Begin();
+            {
+                commandList.SetFramebuffer(device.MainSwapchain.Framebuffer);
+                commandList.ClearColorTarget(0, RgbaFloat.Black);
+                commandList.ClearDepthStencil(1.0f);
+
+                Render(a, b);
+
+                imGuiController.Render(commandList);
+            }
+            commandList.End();
+
+            device.SubmitCommandsAndSwapBuffers(commandList, device.MainSwapchain);
+
+            imGuiController.PlatformSwapBuffers();
+        };
+
+        window.Resize += (a, b) =>
+        {
+            device.MainSwapchain.Resize(b.Width, b.Height);
+
+            Resize(a, b);
+        };
+
+        _device = device;
+        _mainCommandList = commandList;
+
+        window.Run();
+    }
+
+    private static void Load(object? sender, LoadEventArgs e)
+    {
+        Console.WriteLine("Device: " + _device);
+        Console.WriteLine("MainCommandList: " + _mainCommandList);
+    }
+
+    private static void Update(object? sender, UpdateEventArgs e)
+    {
+    }
+
+    private static void Render(object? sender, RenderEventArgs e)
+    {
+    }
+
+    private static void Resize(object? sender, ResizeEventArgs e)
+    {
+    }
+}
