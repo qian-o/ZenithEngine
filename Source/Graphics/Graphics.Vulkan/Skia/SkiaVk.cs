7ad2fabed2b134f4fd1f13e8f7f3659ae52c26bf
王先生 <1324771795@qq.com>
2024-09-18 16:30:38 +0800

    添加 Tests.Skia 项目及 SkiaSharp 支持
    在解决方案中添加了新的 Tests.Skia 项目，并在 Graphics.Core 中添加了对 SkiaSharp 的包引用。Context.cs 文件中新增了 ApiVersion 和 Debugging 静态属性，并调整了 ApplicationInfo 结构体。移除了 SpirvCompilation.cs 中对 Silk.NET.Vulkan 的引用，并使用 Context.ApiVersion。新增 SkiaVk.cs 文件用于创建 SkiaSharp 的 SKSurface。新增 app.manifest 文件设置 DPI 感知属性。新增 Program.cs 文件定义 Tests.Skia 项目的入口点。新增 Tests.Skia.csproj 文件定义项目配置。

---
+++ Source/Graphics/Graphics.Vulkan/Skia/SkiaVk.cs
@@ -0,0 +1,40 @@
+﻿using SkiaSharp;
+
+namespace Graphics.Vulkan;
+
+public static class SkiaVk
+{
+    public static SKSurface CreateSurface(Texture texture)
+    {
+        GRVkBackendContext backendContext = new()
+        {
+            VkInstance = texture.VkRes.Instance.Handle,
+            VkPhysicalDevice = texture.VkRes.VkPhysicalDevice.Handle,
+            VkDevice = texture.VkRes.VkDevice.Handle,
+            VkQueue = texture.VkRes.GraphicsDevice.GraphicsExecutor.Handle.Handle,
+            GraphicsQueueIndex = texture.VkRes.GraphicsDevice.GraphicsExecutor.FamilyIndex,
+            MaxAPIVersion = Context.ApiVersion,
+            GetProcedureAddress = GetProcedureAddress,
+        };
+
+        GRContext context = GRContext.CreateVulkan(backendContext);
+
+        GRVkImageInfo imageInfo = new();
+        imageInfo.Image = texture.Handle.Handle;
+
+        nint GetProcedureAddress(string name, nint instance, nint device)
+        {
+            if (instance != 0)
+            {
+                return texture.VkRes.Vk.GetInstanceProcAddr(texture.VkRes.Instance, name);
+            }
+
+            if (device != 0)
+            {
+                return texture.VkRes.Vk.GetDeviceProcAddr(texture.VkRes.VkDevice, name);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Source/Tests/Tests.Skia/Program.cs
@@ -0,0 +1,103 @@
+﻿using Graphics.Core;
+using Graphics.Vulkan;
+using Tests.Core;
+
+namespace Tests.Skia;
+
+internal sealed unsafe class Program
+{
+    private static GraphicsDevice _device = null!;
+    private static ImGuiController _imGuiController = null!;
+    private static View[] _views = null!;
+
+    private static void Main(string[] _)
+    {
+        using Window window = Window.CreateWindowByVulkan();
+        window.Title = "Tests.Skia";
+        window.MinimumSize = new(100, 100);
+
+        using Context context = new();
+        using GraphicsDevice device = context.CreateGraphicsDevice(context.GetBestPhysicalDevice(), window);
+        using ImGuiController imGuiController = new(window,
+                                                    device,
+                                                    new ImGuiFontConfig("Assets/Fonts/msyh.ttf", 16, (a) => (nint)a.Fonts.GetGlyphRangesChineseFull()),
+                                                    ImGuiSizeConfig.Default);
+
+        using CommandList commandList = device.Factory.CreateGraphicsCommandList();
+
+        window.Load += Load;
+
+        window.Update += (a, b) =>
+        {
+            imGuiController.Update(b.DeltaTime);
+
+            Update(a, b);
+        };
+
+        window.Render += (a, b) =>
+        {
+            Render(a, b);
+
+            commandList.Begin();
+            {
+                commandList.SetFramebuffer(device.MainSwapchain.Framebuffer);
+                commandList.ClearColorTarget(0, RgbaFloat.Black);
+                commandList.ClearDepthStencil(1.0f);
+
+                imGuiController.Render(commandList);
+            }
+            commandList.End();
+
+            device.SubmitCommandsAndSwapBuffers(commandList, device.MainSwapchain);
+
+            imGuiController.PlatformSwapBuffers();
+        };
+
+        window.Resize += (a, b) =>
+        {
+            device.MainSwapchain.Resize(b.Width, b.Height);
+
+            Resize(a, b);
+        };
+
+        window.Closing += Closing;
+
+        _device = device;
+        _imGuiController = imGuiController;
+
+        window.Run();
+    }
+
+    private static void Load(object? sender, LoadEventArgs e)
+    {
+        // _views = [new SkiaView(_device, _imGuiController)];
+    }
+
+    private static void Update(object? sender, UpdateEventArgs e)
+    {
+        foreach (View view in _views)
+        {
+            view.Update(e);
+        }
+    }
+
+    private static void Render(object? sender, RenderEventArgs e)
+    {
+        foreach (View view in _views)
+        {
+            view.Render(e);
+        }
+    }
+
+    private static void Resize(object? sender, ResizeEventArgs e)
+    {
+    }
+
+    private static void Closing(object? sender, ClosingEventArgs e)
+    {
+        foreach (View view in _views)
+        {
+            view.Dispose();
+        }
+    }
+}
