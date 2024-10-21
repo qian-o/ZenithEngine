f8a2849773651ab0aa5e3c7477c196e2529b26ca
王先生 <1324771795@qq.com>
2024-10-21 17:48:52 +0800

    添加 Tests.RayQuery 项目及其基本配置
    在 Renderer.sln 中添加了 Tests.RayQuery 项目，并配置了构建和部署设置。更新了 app.manifest 文件以声明 DPI 感知设置。在 Program.cs 中实现了基本的 Vulkan 图形应用程序，包括窗口创建、图形设备初始化、ImGui 控制器设置及事件处理。配置了 Tests.RayQuery.csproj 项目文件的基本设置。

---
+++ Source/Tests/Tests.RayQuery/Program.cs
@@ -0,0 +1,123 @@
+﻿using Graphics.Core;
+using Graphics.Core.Window;
+using Graphics.Vulkan;
+using Graphics.Vulkan.Descriptions;
+using Graphics.Vulkan.ImGui;
+using Hexa.NET.ImGui;
+using Tests.Core;
+
+namespace Tests.RayQuery;
+
+internal sealed unsafe class Program
+{
+    private static GraphicsDevice _device = null!;
+    private static ImGuiController _imGuiController = null!;
+    private static View[] _views = null!;
+
+    private static void Main(string[] _)
+    {
+        using SdlWindow window = SdlWindow.CreateWindowByVulkan();
+        window.Title = "Tests.RayQuery";
+        window.MinimumSize = new(100, 100);
+
+        using Context context = new();
+        using GraphicsDevice device = context.CreateGraphicsDevice(context.GetBestPhysicalDevice());
+        using Swapchain swapchain = device.Factory.CreateSwapchain(new SwapchainDescription(window.VkSurface!, device.GetBestDepthFormat()));
+        using ImGuiController imGuiController = new(window,
+                                                    device,
+                                                    swapchain.OutputDescription,
+                                                    new ImGuiFontConfig("Assets/Fonts/msyh.ttf", 16, (a) => (nint)a.Fonts.GetGlyphRangesChineseFull()),
+                                                    ImGuiSizeConfig.Default);
+        using CommandList commandList = device.Factory.CreateGraphicsCommandList();
+
+        window.Load += Load;
+
+        window.Update += (a, b) =>
+        {
+            imGuiController.Update(b.DeltaTime);
+
+            Update(a, b);
+        };
+
+        window.Render += (a, b) =>
+        {
+            Render(a, b);
+
+            commandList.Begin();
+            {
+                commandList.SetFramebuffer(swapchain.Framebuffer);
+                commandList.ClearColorTarget(0, RgbaFloat.Black);
+                commandList.ClearDepthStencil(1.0f);
+
+                imGuiController.Render(commandList);
+            }
+            commandList.End();
+
+            device.SubmitCommandsAndSwapBuffers(commandList, swapchain);
+
+            imGuiController.PlatformSwapBuffers();
+        };
+
+        window.Resize += (a, b) =>
+        {
+            swapchain.Resize();
+
+            Resize(a, b);
+        };
+
+        window.Closing += Closing;
+
+        _device = device;
+        _imGuiController = imGuiController;
+
+        window.Run();
+    }
+
+    private static void Load(object? sender, LoadEventArgs e)
+    {
+        _views = [];
+    }
+
+    private static void Update(object? sender, UpdateEventArgs e)
+    {
+        foreach (View view in _views)
+        {
+            view.Update(e);
+        }
+    }
+
+    private static void Render(object? sender, RenderEventArgs e)
+    {
+        foreach (View view in _views)
+        {
+            view.Render(e);
+        }
+
+        ImGui.Begin("Tests.RayQuery");
+        {
+            ImGui.Text($"FPS: {1.0f / e.DeltaTime}");
+
+            ImGui.Separator();
+
+            ImGui.Text($"Total Time: {e.TotalTime}");
+
+            ImGui.Separator();
+
+            ImGui.Text($"Delta Time: {e.DeltaTime}");
+
+            ImGui.End();
+        }
+    }
+
+    private static void Resize(object? sender, ResizeEventArgs e)
+    {
+    }
+
+    private static void Closing(object? sender, ClosingEventArgs e)
+    {
+        foreach (View view in _views)
+        {
+            view.Dispose();
+        }
+    }
+}
