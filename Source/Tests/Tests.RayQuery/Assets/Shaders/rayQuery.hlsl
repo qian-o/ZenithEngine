987e9f0d72135ec823ef127cf7111851e61b0c22
王先生 <1324771795@qq.com>
2024-10-23 18:23:10 +0800

    重构光线查询和环境光遮蔽计算
    重构 `rayQuery.hlsl` 文件中的光线查询和环境光遮蔽计算：
    - 修改 `Light` 结构体。
    - 重新排列 `VSInput` 和 `VSOutput` 结构体的字段。
    - 删除 `mainPS` 函数，添加模板函数 `ProceedRayQuery`。
    - 新增 `calculateAmbientOcclusion` 函数。
    - 重新定义 `mainPS` 函数，增加环境光遮蔽计算。
    - 调整光线查询命中三角形时的颜色计算方式。
    - 最后，颜色计算中增加环境光遮蔽的影响。

--- Source/Tests/Tests.RayQuery/Assets/Shaders/rayQuery.hlsl
+++ Source/Tests/Tests.RayQuery/Assets/Shaders/rayQuery.hlsl
@@ -49,23 +49,46 @@ struct Light
 
 struct VSInput
 {
-    [[vk::location(0)]] float3 Position : POSITION0;
-    [[vk::location(1)]] float3 Normal : NORMAL0;
-    [[vk::location(2)]] float2 TexCoord : TEXCOORD0;
-    [[vk::location(3)]] float3 Color : COLOR0;
-    [[vk::location(4)]] float4 Tangent : TEXCOORD1;
-    [[vk::location(5)]] uint NodeIndex : TEXCOORD2;
+    [[vk::location(0)]]
+    float3 Position : POSITION0;
+
+    [[vk::location(1)]]
+    float3 Normal : NORMAL0;
+
+    [[vk::location(2)]]
+    float2 TexCoord : TEXCOORD0;
+
+    [[vk::location(3)]]
+    float3 Color : COLOR0;
+
+    [[vk::location(4)]]
+    float4 Tangent : TEXCOORD1;
+
+    [[vk::location(5)]]
+    uint NodeIndex : TEXCOORD2;
 };
 
 struct VSOutput
 {
     float4 Position : SV_POSITION;
-    [[vk::location(0)]] float3 WorldPosition : POSITION0;
-    [[vk::location(1)]] float3 Normal : NORMAL0;
-    [[vk::location(2)]] float2 TexCoord : TEXCOORD0;
-    [[vk::location(3)]] float3 Color : COLOR0;
-    [[vk::location(4)]] float4 Tangent : TEXCOORD1;
-    [[vk::location(5)]] nointerpolation uint NodeIndex : TEXCOORD2;
+
+    [[vk::location(0)]]
+    float3 WorldPosition : POSITION0;
+
+    [[vk::location(1)]]
+    float3 Normal : NORMAL0;
+
+    [[vk::location(2)]]
+    float2 TexCoord : TEXCOORD0;
+
+    [[vk::location(3)]]
+    float3 Color : COLOR0;
+
+    [[vk::location(4)]]
+    float4 Tangent : TEXCOORD1;
+
+    [[vk::location(5)]]
+    nointerpolation uint NodeIndex : TEXCOORD2;
 };
 
 RaytracingAccelerationStructure as : register(t0, space0);
@@ -93,43 +116,99 @@ VSOutput mainVS(VSInput input)
     return output;
 }
 
-float4 mainPS(VSOutput input) : SV_TARGET
+template <uint Flags>
+void ProceedRayQuery(RayQuery<Flags> rayQuery, float3 origin, float3 direction, float tmin, float tmax)
 {
-    GeometryNode node = geometryNodes[input.NodeIndex];
-    
-    float4 color = textureArray[node.baseColorTextureIndex].Sample(samplerArray[0], input.TexCoord) * float4(input.Color, 1.0);
-    
-    float3 N = normalize(input.Normal);
-    float3 L = normalize(lights[0].position - input.WorldPosition);
-    float3 V = normalize(-input.WorldPosition);
-    float3 R = normalize(-reflect(L, N));
-    float3 diffuse = max(dot(N, L), 0.1) * color;
-    
-    color = float4(diffuse, 1.0);
-    
     RayDesc ray;
-    ray.Origin = input.WorldPosition;
-    ray.Direction = L;
-    ray.TMin = camera.nearPlane;
-    ray.TMax = camera.farPlane;
-        
-    RayQuery < RAY_FLAG_ACCEPT_FIRST_HIT_AND_END_SEARCH > rayQuery;
+    ray.Origin = origin;
+    ray.Direction = direction;
+    ray.TMin = tmin;
+    ray.TMax = tmax;
+
     rayQuery.TraceRayInline(as, 0, 0xff, ray);
-    
+
     while (rayQuery.Proceed())
     {
         uint candidateType = rayQuery.CandidateType();
-        
+
         if (candidateType == CANDIDATE_NON_OPAQUE_TRIANGLE)
         {
             rayQuery.CommitNonOpaqueTriangleHit();
         }
     }
+}
+
+float calculateAmbientOcclusion(float3 object_point, float3 object_normal)
+{
+    const float ao_mult = 1;
+    uint max_ao_each = 3;
+    uint max_ao = max_ao_each * max_ao_each;
+    const float max_dist = 2;
+    const float tmin = 0.01, tmax = max_dist;
+    float accumulated_ao = 0.f;
+    float3 u = abs(dot(object_normal, float3(0, 0, 1))) > 0.9 ? cross(object_normal, float3(1, 0, 0))
+                                                              : cross(object_normal, float3(0, 0, 1));
+    float3 v = cross(object_normal, u);
+    float accumulated_factor = 0;
+    for (uint j = 0; j < max_ao_each; ++j)
+    {
+        float phi = 0.5 * (-3.14159 + 2 * 3.14159 * (float(j + 1) / float(max_ao_each + 2)));
+        for (uint k = 0; k < max_ao_each; ++k)
+        {
+            float theta = 0.5 * (-3.14159 + 2 * 3.14159 * (float(k + 1) / float(max_ao_each + 2)));
+            float x = cos(phi) * sin(theta);
+            float y = sin(phi) * sin(theta);
+            float z = cos(theta);
+            float3 direction = x * u + y * v + z * object_normal;
+
+            RayQuery<RAY_FLAG_ACCEPT_FIRST_HIT_AND_END_SEARCH> rayQuery;
+            ProceedRayQuery(rayQuery, object_point, direction, tmin, tmax);
+
+            float dist = max_dist;
+            if (rayQuery.CommittedStatus() != COMMITTED_NOTHING)
+            {
+                dist = rayQuery.CommittedRayT();
+            }
+
+            float ao = min(dist, max_dist);
+            float factor = 0.2 + 0.8 * z * z;
+
+            accumulated_factor += factor;
+            accumulated_ao += ao * factor;
+        }
+    }
+
+    accumulated_ao /= (max_dist * accumulated_factor);
+    accumulated_ao *= accumulated_ao;
+    accumulated_ao = max(min((accumulated_ao)*ao_mult, 1), 0);
+
+    return accumulated_ao;
+}
+
+float4 mainPS(VSOutput input) : SV_TARGET
+{
+    GeometryNode node = geometryNodes[input.NodeIndex];
+
+    float4 color =
+        textureArray[node.baseColorTextureIndex].Sample(samplerArray[0], input.TexCoord) * float4(input.Color, 1.0);
+
+    float3 N = normalize(input.Normal);
+    float3 L = normalize(lights[0].position - input.WorldPosition);
+    float3 V = normalize(-input.WorldPosition);
+    float3 R = normalize(-reflect(L, N));
+    float3 diffuse = max(dot(N, L), 0.1) * color;
+
+    color = float4(diffuse, 1.0);
+
+    RayQuery<RAY_FLAG_ACCEPT_FIRST_HIT_AND_END_SEARCH> rayQuery;
+    ProceedRayQuery(rayQuery, input.WorldPosition, L, camera.nearPlane, camera.farPlane);
 
     if (rayQuery.CommittedStatus() == COMMITTED_TRIANGLE_HIT)
     {
-        color.rgb *= 0.1;
+        color.rgb *= 0.3;
     }
-    
+
+    color.rgb *= calculateAmbientOcclusion(input.WorldPosition, N);
+
     return color;
 }
