58d465015f56403eee294d3646505265ae66daef
王先生 <1324771795@qq.com>
2024-11-11 11:12:30 +0800

    Add CommandBuffer and CommandProcessor classes
    Introduce a new namespace `Graphics.Engine` with abstract classes `CommandBuffer` and `CommandProcessor` for handling command buffers and processors. These classes inherit from `DeviceResource` and include methods for beginning, ending, submitting, and waiting for commands. Additionally, add `CommandProcessorType` enum in `Graphics.Engine.Enums` to categorize processor types (Graphics, Compute, Transfer).

---
+++ Source/Graphics/Graphics.Engine/CommandBuffer.cs
@@ -0,0 +1,13 @@
+﻿namespace Graphics.Engine;
+
+public abstract class CommandBuffer(Context context) : DeviceResource(context)
+{
+    public abstract void Begin();
+
+    public void End()
+    {
+        EndInternal();
+    }
+
+    protected abstract void EndInternal();
+}
---
+++ Source/Graphics/Graphics.Engine/CommandProcessor.cs
@@ -0,0 +1,20 @@
+﻿namespace Graphics.Engine;
+
+public abstract class CommandProcessor(Context context) : DeviceResource(context)
+{
+    /// <summary>
+    /// Gets the next available command buffer.
+    /// </summary>
+    /// <returns></returns>
+    public abstract CommandBuffer CommandBuffer();
+
+    /// <summary>
+    /// Submits the command buffer to be executed by the GPU.
+    /// </summary>
+    public abstract void Submit();
+
+    /// <summary>
+    /// Waits for the GPU to finish executing all commands.
+    /// </summary>
+    public abstract void WaitIdle();
+}
