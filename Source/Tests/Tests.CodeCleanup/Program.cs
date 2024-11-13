36ba1ba09b7e35d9ae775851add61193e346eb32
王先生 <1324771795@qq.com>
2024-11-13 11:52:35 +0800

    添加 Tests.CodeCleanup 项目及其配置
    添加了一个新的项目 `Tests.CodeCleanup` 到解决方案文件 `Renderer.sln` 中。
    在 `Renderer.sln` 的 `GlobalSection(SolutionConfigurationPlatforms)` 部分中，为新项目 `Tests.CodeCleanup` 添加了调试和发布配置。
    在 `Renderer.sln` 的 `GlobalSection(NestedProjects)` 部分中，添加了新项目 `Tests.CodeCleanup` 的嵌套项目配置。
    新增了 `Program.cs` 文件，定义了 `Tests.CodeCleanup` 命名空间，并包含一个 `Program` 类。该类包含 `Main` 方法、`ReturnOne` 方法和 `TestIf` 方法。
    新增了 `Tests.CodeCleanup.csproj` 文件，定义了项目的 SDK、输出类型、目标框架以及启用隐式使用和可空引用类型的属性。

---
+++ Source/Tests/Tests.CodeCleanup/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Tests.CodeCleanup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            ReturnOne();
+
+            TestIf(true);
+        }
+
+        static int ReturnOne()
+        {
+            return 1;
+        }
+
+        static void TestIf(bool? a)
+        {
+            if (a == null)
+            {
+                throw new ArgumentNullException(nameof(a));
+            }
+
+            if (a == true)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
+
