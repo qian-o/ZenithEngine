1e8dd87342b510e8be36639ebde78888b0bc9dcc
王先生 <1324771795@qq.com>
2024-11-13 17:54:39 +0800

    实现持久化内存分配器和基础资源释放机制
    在 `Allocator.cs` 文件中：
    - 引入 `System.Runtime.InteropServices` 命名空间。
    - 定义 `ZenithEngine.Common` 命名空间。
    - 实现 `Allocator` 不安全类，继承自 `DisposableObject`。
    - 提供内存分配和释放方法，包括 `Alloc` 和 `Free` 系列方法。
    - 实现 `Release` 和 `Destroy` 方法。
    
    在 `DisposableObject.cs` 文件中：
    - 定义 `ZenithEngine.Common` 命名空间。
    - 实现 `DisposableObject` 抽象类，继承自 `IDisposable`。
    - 提供资源释放机制，包括析构函数、`IsDisposed` 属性、`Dispose` 方法和抽象 `Destroy` 方法。

---
+++ src/ZenithEngine.Common/Allocator.cs
@@ -0,0 +1,105 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ZenithEngine.Common;
+
+/// <summary>
+/// Provides a persistent memory allocator.
+/// </summary>
+public unsafe class Allocator : DisposableObject
+{
+    private readonly Lock @lock = new();
+    private readonly List<nint> blocks = [];
+
+    public void* Alloc(uint size)
+    {
+        @lock.Enter();
+
+        void* ptr = NativeMemory.Alloc(size);
+
+        blocks.Add((nint)ptr);
+
+        @lock.Exit();
+
+        return ptr;
+    }
+
+    public T* Alloc<T>() where T : unmanaged
+    {
+        return (T*)Alloc((uint)sizeof(T));
+    }
+
+    public T* Alloc<T>(int count) where T : unmanaged
+    {
+        return (T*)Alloc((uint)sizeof(T) * count);
+    }
+
+    public T* Alloc<T>(params T[] values) where T : unmanaged
+    {
+        T* ptr = Alloc<T>(values.Length);
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            ptr[i] = values[i];
+        }
+
+        return ptr;
+    }
+
+    public char** Alloc(params string[] values)
+    {
+        nint* ptr = Alloc<nint>(values.Length);
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            char* str = Alloc<char>(values[i].Length + 1);
+
+            for (int j = 0; j < values[i].Length; j++)
+            {
+                str[j] = values[i][j];
+            }
+
+            str[values[i].Length] = '\0';
+
+            ptr[i] = (nint)str;
+        }
+
+        return (char**)ptr;
+    }
+
+    public void Free(void* ptr)
+    {
+        blocks.Remove((nint)ptr);
+
+        NativeMemory.Free(ptr);
+    }
+
+    public void Free<T>(T* ptr) where T : unmanaged
+    {
+        Free((void*)ptr);
+    }
+
+    public void Free<T>(T** ptr, int count) where T : unmanaged
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Free(ptr[i]);
+        }
+
+        Free(ptr);
+    }
+
+    public void Release()
+    {
+        for (int i = 0; i < blocks.Count; i++)
+        {
+            NativeMemory.Free((void*)blocks[i]);
+        }
+
+        blocks.Clear();
+    }
+
+    protected override void Destroy()
+    {
+        Release();
+    }
+}
