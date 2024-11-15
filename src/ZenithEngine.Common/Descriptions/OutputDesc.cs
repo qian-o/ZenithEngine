5558be9e50e3fb54ed81e7b71e53f1c1e2889394
王先生 <1324771795@qq.com>
2024-11-16 00:33:36 +0800

    添加多个新结构体和方法以增强图形描述功能
    在 `Utils.cs` 文件中：
    * 添加了 `using ZenithEngine.Common.Enums;` 语句。
    * 在 `Utils` 类中添加了一个新的静态方法 `GetFormatSizeInBytes`，用于根据 `ElementFormat` 返回对应的字节大小。
    
    在 `ElementDesc.cs` 文件中：
    * 添加了 `using ZenithEngine.Common.Enums;` 语句。
    * 新增了 `ElementDesc` 结构体，包含 `Format`、`Semantic`、`SemanticIndex` 和 `Offset` 属性。
    * 添加了一个静态方法 `Default`，用于创建默认的 `ElementDesc` 实例。
    
    在 `GraphicsShaderDesc.cs` 文件中：
    * 添加了 `using ZenithEngine.Common.Graphics;` 语句。
    * 新增了 `GraphicsShaderDesc` 结构体，包含 `Vertex`、`Hull`、`Domain`、`Geometry` 和 `Pixel` 属性。
    * 添加了一个静态方法 `Default`，用于创建默认的 `GraphicsShaderDesc` 实例。
    
    在 `LayoutDesc.cs` 文件中：
    * 添加了 `using ZenithEngine.Common.Enums;` 语句。
    * 新增了 `LayoutDesc` 结构体，包含 `Elements`、`StepFunction`、`StepRate` 和 `Stride` 属性。
    * 添加了一个静态方法 `Default`，用于创建默认的 `LayoutDesc` 实例。
    * 添加了一个方法 `Add`，用于向 `Elements` 数组中添加新的 `ElementDesc` 元素，并更新 `Stride`。
    
    在 `OutputDesc.cs` 文件中：
    * 添加了 `using ZenithEngine.Common.Enums;` 语句。
    * 新增了 `OutputDesc` 结构体，包含 `ColorAttachments`、`DepthStencilAttachment` 和 `SampleCount` 属性。
    * 添加了一个静态方法 `Default`，用于创建默认的 `OutputDesc` 实例。

---
+++ src/ZenithEngine.Common/Descriptions/OutputDesc.cs
@@ -0,0 +1,33 @@
+﻿using ZenithEngine.Common.Enums;
+
+namespace ZenithEngine.Common.Descriptions;
+
+public struct OutputDesc
+{
+    /// <summary>
+    /// Color attachment formats.
+    /// </summary>
+    public PixelFormat[] ColorAttachments { get; set; }
+
+    /// <summary>
+    /// Depth stencil attachment format.
+    /// </summary>
+    public PixelFormat? DepthStencilAttachment { get; set; }
+
+    /// <summary>
+    /// The number of samples in each target attachment.
+    /// </summary>
+    public TextureSampleCount SampleCount { get; set; }
+
+    public static OutputDesc Default(TextureSampleCount sampleCount = TextureSampleCount.Count1,
+                                     PixelFormat? depthStencilAttachment = null,
+                                     params PixelFormat[] colorAttachments)
+    {
+        return new()
+        {
+            ColorAttachments = colorAttachments,
+            DepthStencilAttachment = depthStencilAttachment,
+            SampleCount = sampleCount
+        };
+    }
+}
