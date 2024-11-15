7970242a370134080477cdd06f3e78311a99bd5b
王先生 <1324771795@qq.com>
2024-11-15 23:58:47 +0800

    添加 SamplerDesc 结构体和 Sampler 抽象类
    在 SamplerDesc.cs 中添加了 using 语句和命名空间，并新增了 SamplerDesc 结构体，包含多个属性和一个静态方法 Default，用于描述采样器的各种配置。
    在 Sampler.cs 中添加了 using 语句和命名空间，并新增了一个抽象类 Sampler，继承自 GraphicsResource，包含一个只读属性 Desc，用于保存 SamplerDesc 的描述信息。

---
+++ src/ZenithEngine.Common/Graphics/Sampler.cs
@@ -0,0 +1,9 @@
+﻿using ZenithEngine.Common.Descriptions;
+
+namespace ZenithEngine.Common.Graphics;
+
+public abstract class Sampler(GraphicsContext context,
+                              ref readonly SamplerDesc desc) : GraphicsResource(context)
+{
+    public SamplerDesc Desc { get; } = desc;
+}
