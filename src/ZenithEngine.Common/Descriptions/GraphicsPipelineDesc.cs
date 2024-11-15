ac6f9005671270989de3705e428488020c3ad167
王先生 <1324771795@qq.com>
2024-11-16 00:39:35 +0800

    重构图形管线描述和管线类
    在 `GraphicsPipelineDesc.cs` 文件中：
    - 修改命名空间为 `using ZenithEngine.Common.Enums;` 和 `using ZenithEngine.Common.Graphics;`。
    - 添加 `GraphicsPipelineDesc` 结构体的多个属性：`RenderStates`、`Shaders`、`InputLayouts`、`ResourceLayouts`、`PrimitiveTopology` 和 `Outputs`。
    - 添加静态方法 `Default`，用于创建默认的 `GraphicsPipelineDesc` 实例。
    
    在 `GraphicsPipeline.cs` 文件中：
    - 添加 `using ZenithEngine.Common.Descriptions;`。
    - 定义抽象类 `GraphicsPipeline`，继承自 `Pipeline`，并包含只读属性 `Desc`。
    
    在 `Pipeline.cs` 文件中：
    - 添加命名空间 `ZenithEngine.Common.Graphics;`。
    - 定义抽象类 `Pipeline`，继承自 `GraphicsResource`。

--- src/ZenithEngine.Common/Descriptions/GraphicsPipelineDesc.cs
+++ src/ZenithEngine.Common/Descriptions/GraphicsPipelineDesc.cs
@@ -1,5 +1,59 @@
-﻿namespace ZenithEngine.Common.Descriptions;
+﻿using ZenithEngine.Common.Enums;
+using ZenithEngine.Common.Graphics;
+
+namespace ZenithEngine.Common.Descriptions;
 
 public struct GraphicsPipelineDesc
 {
+    /// <summary>
+    /// The render state description.
+    /// </summary>
+    public RenderStateDesc RenderStates { get; set; }
+
+    /// <summary>
+    /// The shader state description.
+    /// </summary>
+    public GraphicsShaderDesc Shaders { get; set; }
+
+    /// <summary>
+    /// vertex input layout.
+    /// </summary>
+    public LayoutDesc[] InputLayouts { get; set; }
+
+    /// <summary>
+    /// Describes the resource layouts input array.
+    /// </summary>
+    public ResourceLayout[] ResourceLayouts { get; set; }
+
+    /// <summary>
+    /// Define how vertices are interpreted and rendered by the pipeline.
+    /// </summary>
+    public PrimitiveTopology PrimitiveTopology { get; set; }
+
+    /// <summary>
+    /// A description of the output attachments of the pipeline.
+    /// </summary>
+    public OutputDesc Outputs { get; set; }
+
+    public static GraphicsPipelineDesc Default(LayoutDesc[] inputLayouts,
+                                               OutputDesc outputs,
+                                               RenderStateDesc? renderStates = null,
+                                               GraphicsShaderDesc? shaders = null,
+                                               ResourceLayout[]? resourceLayouts = null,
+                                               PrimitiveTopology primitiveTopology = PrimitiveTopology.TriangleList)
+    {
+        renderStates ??= RenderStateDesc.Default();
+        shaders ??= GraphicsShaderDesc.Default();
+        resourceLayouts ??= [];
+
+        return new()
+        {
+            RenderStates = renderStates.Value,
+            Shaders = shaders.Value,
+            InputLayouts = inputLayouts,
+            ResourceLayouts = resourceLayouts,
+            PrimitiveTopology = primitiveTopology,
+            Outputs = outputs
+        };
+    }
 }
---
+++ src/ZenithEngine.Common/Graphics/GraphicsPipeline.cs
@@ -0,0 +1,9 @@
+﻿using ZenithEngine.Common.Descriptions;
+
+namespace ZenithEngine.Common.Graphics;
+
+public abstract class GraphicsPipeline(GraphicsContext context,
+                                       ref readonly GraphicsPipelineDesc desc) : Pipeline(context)
+{
+    public GraphicsPipelineDesc Desc { get; } = desc;
+}
