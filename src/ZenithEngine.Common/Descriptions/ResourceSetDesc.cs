50a07b03f9d484133edcc60c6486e66093672ce3
王先生 <1324771795@qq.com>
2024-11-15 23:55:46 +0800

    引入资源布局和资源集的结构体和类
    这些更改引入了几个新的结构体和类，用于描述和管理资源布局和资源集。
    
    在 `LayoutElementDesc.cs` 中：
    * 添加了 `ZenithEngine.Common.Enums` 的引用。
    * 定义了 `ZenithEngine.Common.Descriptions` 命名空间。
    * 新增了 `LayoutElementDesc` 结构体，用于描述布局元素，包括槽位、资源类型、着色器阶段、元素选项和大小。
    * 提供了一个静态方法 `Default`，用于创建默认的 `LayoutElementDesc` 实例。
    
    在 `ResourceLayoutDesc.cs` 中：
    * 定义了 `ZenithEngine.Common.Descriptions` 命名空间。
    * 新增了 `ResourceLayoutDesc` 结构体，用于描述资源布局，包括一个 `LayoutElementDesc` 数组。
    * 提供了一个静态方法 `Default`，用于创建默认的 `ResourceLayoutDesc` 实例。
    
    在 `ResourceSetDesc.cs` 中：
    * 添加了 `ZenithEngine.Common.Graphics` 的引用。
    * 定义了 `ZenithEngine.Common.Descriptions` 命名空间。
    * 新增了 `ResourceSetDesc` 结构体，用于描述资源集，包括资源布局和资源数组。
    * 提供了一个静态方法 `Default`，用于创建默认的 `ResourceSetDesc` 实例。
    
    在 `ResourceLayout.cs` 中：
    * 添加了 `ZenithEngine.Common.Descriptions` 的引用。
    * 定义了 `ZenithEngine.Common.Graphics` 命名空间。
    * 新增了 `ResourceLayout` 抽象类，继承自 `GraphicsResource`，用于表示资源布局，并包含一个 `ResourceLayoutDesc` 描述。
    
    在 `ResourceSet.cs` 中：
    * 添加了 `ZenithEngine.Common.Descriptions` 的引用。
    * 定义了 `ZenithEngine.Common.Graphics` 命名空间。
    * 新增了 `ResourceSet` 抽象类，继承自 `GraphicsResource`，用于表示资源集，并包含一个 `ResourceSetDesc` 描述。

---
+++ src/ZenithEngine.Common/Descriptions/ResourceSetDesc.cs
@@ -0,0 +1,26 @@
+﻿using ZenithEngine.Common.Graphics;
+
+namespace ZenithEngine.Common.Descriptions;
+
+public struct ResourceSetDesc
+{
+    /// <summary>
+    /// The layout of the resource set.
+    /// </summary>
+    public ResourceLayout Layout { get; set; }
+
+    /// <summary>
+    /// An array of resources that are bound to the resource set.
+    /// </summary>
+    public GraphicsResource[] Resources { get; set; }
+
+    public static ResourceSetDesc Default(ResourceLayout layout,
+                                          params GraphicsResource[] resources)
+    {
+        return new()
+        {
+            Layout = layout,
+            Resources = resources
+        };
+    }
+}
---
+++ src/ZenithEngine.Common/Graphics/ResourceLayout.cs
@@ -0,0 +1,10 @@
+﻿using ZenithEngine.Common.Descriptions;
+
+namespace ZenithEngine.Common.Graphics;
+
+public abstract class ResourceLayout(GraphicsContext context,
+                                     ref readonly ResourceLayoutDesc desc) : GraphicsResource(context)
+{
+    public ResourceLayoutDesc Desc { get; } = desc;
+}
+
