26f128bd3ab3cff31afd39c3851147779687fc39
王先生 <1324771795@qq.com>
2024-11-20 13:42:15 +0800

    FrameBuffer 抽象类中添加了新的抽象属性 Output
    在 `FrameBuffer.cs` 文件中，`FrameBuffer` 抽象类中添加了一个新的抽象属性 `Output`。

--- src/ZenithEngine.Common/Graphics/FrameBuffer.cs
+++ src/ZenithEngine.Common/Graphics/FrameBuffer.cs
@@ -10,4 +10,6 @@ public abstract class FrameBuffer(GraphicsContext context,
     public abstract uint Width { get; }
 
     public abstract uint Height { get; }
+
+    public abstract OutputDesc Output { get; }
 }
