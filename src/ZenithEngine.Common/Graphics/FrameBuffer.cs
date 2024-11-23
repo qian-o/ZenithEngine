adfe29edb2d6faf53d5f1fd0eccaa62a9fa01231
王先生 <1324771795@qq.com>
2024-11-23 23:43:53 +0800

    添加 Silk.NET.Maths 引用并更新抽象属性
    删除 Width 和 Height 抽象属性，新增索引器属性 this[int index] 返回颜色附件大小。为 Output 属性添加 XML 文档注释。

--- src/ZenithEngine.Common/Graphics/FrameBuffer.cs
+++ src/ZenithEngine.Common/Graphics/FrameBuffer.cs
@@ -1,4 +1,5 @@
-﻿using ZenithEngine.Common.Descriptions;
+﻿using Silk.NET.Maths;
+using ZenithEngine.Common.Descriptions;
 
 namespace ZenithEngine.Common.Graphics;
 
@@ -7,9 +8,15 @@ public abstract class FrameBuffer(GraphicsContext context,
 {
     public FrameBufferDesc Desc { get; } = desc;
 
-    public abstract uint Width { get; }
+    /// <summary>
+    /// Get the color attachment size at the specified index.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public abstract Vector2D<uint> this[int index] { get; }
 
-    public abstract uint Height { get; }
-
+    /// <summary>
+    /// Get the output description for graphics pipeline creation.
+    /// </summary>
     public abstract OutputDesc Output { get; }
 }
