f04f89b29afe0a71fb2b495c46647596ea18f720
王先生 <1324771795@qq.com>
2024-11-28 23:48:43 +0800

    添加 Vulkan 测试项目并配置并行测试
    在解决方案中添加了 `ZenithEngine.Vulkan.Test` 项目，并配置了调试和发布设置。新增 `GraphicsContextTest` 测试类及其测试方法 `TestCreateDevice`。在 `MSTestSettings.cs` 中添加了方法级别的并行测试配置。创建了 `ZenithEngine.Vulkan.Test.csproj` 文件，配置了项目属性和引用。

---
+++ src/Tests/ZenithEngine.Vulkan.Test/GraphicsContextTest.cs
@@ -0,0 +1,23 @@
+﻿using ZenithEngine.Common.Enums;
+using ZenithEngine.Common.Graphics;
+
+namespace ZenithEngine.Vulkan.Test;
+
+[TestClass]
+public sealed class GraphicsContextTest
+{
+    [TestMethod]
+    public void TestCreateDevice()
+    {
+		try
+        {
+            using GraphicsContext context = GraphicsContext.Create(Backend.Vulkan);
+
+            context.CreateDevice(true);
+        }
+		catch (Exception)
+		{
+            Assert.Fail();
+        }
+    }
+}
