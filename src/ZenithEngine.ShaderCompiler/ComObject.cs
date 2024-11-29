040a844c0b864e2228b8cd705fd22e9ef7bd7008
王先生 <1324771795@qq.com>
2024-11-29 22:08:46 +0800

    重构和优化代码，增强可维护性和可扩展性
    重构了 `IncludeHandler.cs`，添加命名空间引用，并将 `IncludeHandler` 类从继承 `ObjectInterface` 改为继承新的 `ComObject` 类。重构了构造函数和 `LoadSource` 方法。
    
    删除了 `ObjectHandle.cs` 中的 `ObjectHandle` 结构体。
    
    删除了 `ObjectInterface.cs` 中的 `ObjectInterface` 类及其相关方法和常量。
    
    删除了 `VTable.cs` 中的 `VTable` 结构体。
    
    新增 `ComObject.cs` 文件，定义了新的抽象类 `ComObject`，包含 `ObjectHandle` 结构体、常量、构造函数、虚方法 `InitVTable` 以及 `QueryInterface`、`AddRef` 和 `RemoveRef` 方法。
    
    通过这些改动，代码结构更加清晰，职责分离更明确，增强了代码的可维护性和可扩展性。

---
+++ src/ZenithEngine.ShaderCompiler/ComObject.cs
@@ -0,0 +1,104 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using ZenithEngine.Common;
+
+namespace ZenithEngine.ShaderCompiler;
+
+public abstract unsafe class ComObject : DisposableObject
+{
+    public struct ObjectHandle
+    {
+        public nint LpVtbl;
+
+        public GCHandle ManagedHandle;
+
+        public readonly T GetObject<T>() where T : ComObject
+        {
+            return (T)ManagedHandle.Target!;
+        }
+    }
+
+    public const int Ok = 0;
+    public const int NoInterface = -2147467262;
+    public const int UnspecifiedFailure = -2147467259;
+
+    private readonly Guid guid;
+    private readonly MemoryAllocator allocator;
+
+    private volatile int refCount = 1;
+
+    public ObjectHandle* Handle;
+
+    protected ComObject(int additionalVTableSlots)
+    {
+        guid = Guid.ParseExact(GetType().GetCustomAttribute<GuidAttribute>()!.Value, "D");
+        allocator = new();
+
+        Handle = allocator.Alloc<ObjectHandle>();
+        Handle->LpVtbl = (nint)allocator.Alloc((uint)((3 + additionalVTableSlots) * sizeof(nint)));
+        Handle->ManagedHandle = GCHandle.Alloc(this);
+
+        ((delegate* unmanaged[Stdcall]<ObjectHandle*, Guid*, void**, int>*)Handle->LpVtbl)[0] = &QueryInterface;
+        ((delegate* unmanaged[Stdcall]<ObjectHandle*, ulong>*)Handle->LpVtbl + sizeof(nint))[0] = &AddRef;
+        ((delegate* unmanaged[Stdcall]<ObjectHandle*, ulong>*)Handle->LpVtbl + (sizeof(nint) * 2))[0] = &RemoveRef;
+
+        InitVTable();
+    }
+
+    protected nint GetVTableSlot(int slot)
+    {
+        return Handle->LpVtbl + ((slot + 3) * sizeof(nint));
+    }
+
+    protected abstract void InitVTable();
+
+    protected override void Destroy()
+    {
+        Handle->ManagedHandle.Free();
+
+        allocator.Dispose();
+    }
+
+    [UnmanagedCallersOnly(CallConvs = [typeof(CallConvStdcall)])]
+    private static int QueryInterface(ObjectHandle* pSelf, Guid* pInterfaceId, void** pInterface)
+    {
+        try
+        {
+            ComObject self = pSelf->GetObject<ComObject>();
+
+            if (pInterfaceId[0] == self.guid)
+            {
+                pInterface[0] = self.Handle;
+
+                return Ok;
+            }
+
+            return NoInterface;
+        }
+        catch
+        {
+            return UnspecifiedFailure;
+        }
+    }
+
+    [UnmanagedCallersOnly(CallConvs = [typeof(CallConvStdcall)])]
+    private static ulong AddRef(ObjectHandle* pSelf)
+    {
+        ComObject self = pSelf->GetObject<ComObject>();
+
+        Interlocked.Add(ref self.refCount, 1);
+
+        return (ulong)self.refCount;
+    }
+
+    [UnmanagedCallersOnly(CallConvs = [typeof(CallConvStdcall)])]
+    private static ulong RemoveRef(ObjectHandle* pSelf)
+    {
+        ComObject self = pSelf->GetObject<ComObject>();
+
+        Interlocked.Add(ref self.refCount, -1);
+
+        return (ulong)self.refCount;
+    }
+}
--- src/ZenithEngine.ShaderCompiler/IncludeHandler.cs
+++ src/ZenithEngine.ShaderCompiler/IncludeHandler.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Silk.NET.Core.Native;
 using Silk.NET.Direct3D.Compilers;
@@ -7,36 +6,26 @@ using ZenithEngine.Common;
 
 namespace ZenithEngine.ShaderCompiler;
 
-internal unsafe class IncludeHandler : ObjectInterface
+[Guid("7f61fc7d-950d-467f-b3e3-3c02fb49187c")]
+internal unsafe class IncludeHandler(Func<string, byte[]>? includeHandler) : ComObject(1)
 {
-    private readonly Func<string, byte[]> handler;
+    public Func<string, byte[]> Handler { get; } = includeHandler ?? ((_) => []);
 
-    public IncludeHandler(Func<string, byte[]>? includeHandler) : base(new("7f61fc7d-950d-467f-b3e3-3c02fb49187c"), 1)
+    protected override void InitVTable()
     {
-        handler = includeHandler ?? (_ => []);
-
-        ((delegate* unmanaged[Stdcall]<ObjectHandle*, nint, IDxcBlob**, int>*)AdditionalVTableSlots)[0] = &LoadSource;
+        ((delegate* unmanaged[Stdcall]<ObjectHandle*, nint, IDxcBlob**, int>*)GetVTableSlot(0))[0] = &LoadSource;
     }
 
     [UnmanagedCallersOnly(CallConvs = [typeof(CallConvStdcall)])]
     private static int LoadSource(ObjectHandle* pSelf, nint pFileName, IDxcBlob** includeSourceBlob)
     {
-        try
-        {
-            IncludeHandler self = pSelf->GetObject<IncludeHandler>();
+        byte[] data = pSelf->GetObject<IncludeHandler>().Handler.Invoke(Utils.PtrToStringUni(pFileName));
 
-            byte[] data = self.handler.Invoke(Utils.PtrToStringUni(pFileName));
+        ComPtr<IDxcBlobEncoding> blob = default;
+        DxcCompiler.DxcUtils.CreateBlob(ref data[0], (uint)data.Length, DXC.CPUtf8, ref blob);
 
-            ComPtr<IDxcBlobEncoding> blob = default;
-            DxcCompiler.DxcUtils.CreateBlob(ref data[0], (uint)data.Length, DXC.CPUtf8, ref blob);
+        includeSourceBlob[0] = (IDxcBlob*)(IDxcBlobEncoding*)blob;
 
-            includeSourceBlob[0] = (IDxcBlob*)(IDxcBlobEncoding*)blob;
-
-            return HResultOk;
-        }
-        catch
-        {
-            return HResultUnspecifiedFailure;
-        }
+        return Ok;
     }
 }
