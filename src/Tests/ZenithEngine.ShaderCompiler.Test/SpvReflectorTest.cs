c702815ec575b19d597087dea135b928a6a186b0
王先生 <1324771795@qq.com>
2024-12-02 14:05:55 +0800

    重命名 ReflectResourceLayout 为 ReflectResult
    代码中的 ReflectResourceLayout 类被重命名为 ReflectResult，并且相关的变量名和方法调用也进行了相应的修改。在 SpvReflectorTest.cs 文件中，所有对 ReflectResourceLayout 类的引用都被替换为 ReflectResult。删除了 ReflectResourceLayout.cs 文件中的 ReflectResourceLayout 类。在 SpvReflector.cs 文件中，Reflect 方法的返回类型从 ReflectResourceLayout 改为 ReflectResult。新增 ReflectResult.cs 文件，定义了新的 ReflectResult 类，包含了原 ReflectResourceLayout 类的所有功能。

--- src/Tests/ZenithEngine.ShaderCompiler.Test/SpvReflectorTest.cs
+++ src/Tests/ZenithEngine.ShaderCompiler.Test/SpvReflectorTest.cs
@@ -15,49 +15,49 @@ public class SpvReflectorTest
         byte[] vs = DxcCompiler.Compile(ShaderStages.Vertex, source, "VSMain");
         byte[] ps = DxcCompiler.Compile(ShaderStages.Pixel, source, "PSMain");
 
-        ReflectResourceLayout layout = SpvReflector.Reflect(vs);
-        layout = layout.Merge(SpvReflector.Reflect(ps));
+        ReflectResult result = SpvReflector.Reflect(vs);
+        result = result.Merge(SpvReflector.Reflect(ps));
 
         // ConstantBuffer<MVP> mvp1 : register(b0);
         AssertEx.AreEqual(ShaderStages.Vertex,
                           ResourceType.ConstantBuffer, 0, 0,
                           "mvp1", 1,
-                          layout["mvp1"]);
+                          result["mvp1"]);
 
         // StructuredBuffer<float4x4> mvp2 : register(t0);
         AssertEx.AreEqual(ShaderStages.Vertex | ShaderStages.Pixel,
                           ResourceType.StructuredBuffer, 0, 0,
                           "mvp2", 1,
-                          layout["mvp2"]);
+                          result["mvp2"]);
 
         // RWStructuredBuffer<float4> colors : register(u0, space1);
         AssertEx.AreEqual(ShaderStages.Pixel,
                           ResourceType.StructuredBufferReadWrite, 0, 1,
                           "colors", 1,
-                          layout["colors"]);
+                          result["colors"]);
 
         // RWTexture2D<float4> texture : register(u1, space1);
         AssertEx.AreEqual(ShaderStages.Pixel,
                           ResourceType.TextureReadWrite, 1, 1,
                           "texture", 1,
-                          layout["texture"]);
+                          result["texture"]);
 
         // Texture2D texture1[10] : register(t0, space1);
         AssertEx.AreEqual(ShaderStages.Pixel,
                           ResourceType.Texture, 0, 1,
                           "texture1", 10,
-                          layout["texture1"]);
+                          result["texture1"]);
 
         // Texture2D texture2[] : register(t1, space1);
         AssertEx.AreEqual(ShaderStages.Pixel,
                           ResourceType.Texture, 1, 1,
                           "texture2", 100,
-                          layout["texture2", 100]);
+                          result["texture2", 100]);
 
         // SamplerState samplerState[] : register(s0, space2);
         AssertEx.AreEqual(ShaderStages.Pixel,
                           ResourceType.Sampler, 0, 2,
                           "samplerState", 4,
-                          layout["samplerState", 4]);
+                          result["samplerState", 4]);
     }
 }
--- src/ZenithEngine.ShaderCompiler/SpvReflector.cs
+++ src/ZenithEngine.ShaderCompiler/SpvReflector.cs
@@ -8,7 +8,7 @@ public static unsafe class SpvReflector
 {
     private static readonly SpvReflect reflect = SpvReflect.GetApi();
 
-    public static ReflectResourceLayout Reflect(byte[] shader)
+    public static ReflectResult Reflect(byte[] shader)
     {
         ReflectShaderModule* module = stackalloc ReflectShaderModule[1];
         reflect.CreateShaderModule((uint)shader.Length, ref shader[0], module);
