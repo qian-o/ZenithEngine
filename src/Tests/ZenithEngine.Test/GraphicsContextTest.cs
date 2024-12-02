6037e8b011ba169b2cced6d5e02e36d2108083b6
王先生 <1324771795@qq.com>
2024-12-02 15:40:45 +0800

    添加测试和着色器文件，改进错误捕获
    在 `GraphicsContextTest.cs` 中：
    - 添加对 `ZenithEngine.Common.Descriptions` 和 `ZenithEngine.ShaderCompiler` 的引用。
    - 添加 `assetsPath` 常量。
    - 修改 `TestCreateDevice` 测试方法，使用 `AssertEx.IsConsoleErrorEmpty` 方法。
    - 扩展 `TestShader` 测试方法，增加对着色器编译、反射和资源布局的测试。
    
    在 `ZenithEngine.Test.csproj` 中：
    - 添加 `ItemGroup`，将 `Assets` 目录下的文件设置为在构建时复制到输出目录。
    
    在 `AssertEx.cs` 中：
    - 添加 `AssertEx` 类，包含静态方法 `IsConsoleErrorEmpty`。
    
    在 `Simple.hlsl` 中：
    - 添加新的 HLSL 着色器文件，定义顶点和像素着色器及相关结构体和缓冲区。
    - 实现顶点着色器 `VSMain` 和像素着色器 `PSMain`。

--- src/Tests/ZenithEngine.Test/GraphicsContextTest.cs
+++ src/Tests/ZenithEngine.Test/GraphicsContextTest.cs
@@ -1,5 +1,7 @@
-﻿using ZenithEngine.Common.Enums;
+﻿using ZenithEngine.Common.Descriptions;
+using ZenithEngine.Common.Enums;
 using ZenithEngine.Common.Graphics;
+using ZenithEngine.ShaderCompiler;
 
 namespace ZenithEngine.Test;
 
@@ -8,26 +10,71 @@ public class GraphicsContextTest
 {
     public const Backend RenderBackend = Backend.Vulkan;
 
+    private static readonly string assetsPath = Path.Combine(AppContext.BaseDirectory, "Assets");
+
     [TestMethod]
     public void TestCreateDevice()
     {
-        try
+        AssertEx.IsConsoleErrorEmpty(() =>
         {
             using GraphicsContext context = GraphicsContext.Create(RenderBackend);
 
             context.CreateDevice(true);
-        }
-        catch (Exception)
-        {
-            Assert.Fail();
-        }
+        });
     }
 
     [TestMethod]
     public void TestShader()
     {
-        using GraphicsContext context = GraphicsContext.Create(RenderBackend);
+        AssertEx.IsConsoleErrorEmpty(() =>
+        {
+            string source = File.ReadAllText(Path.Combine(assetsPath, "Simple.hlsl"));
 
-        context.CreateDevice(true);
+            byte[] vs = DxcCompiler.Compile(ShaderStages.Vertex, source, "VSMain");
+            byte[] ps = DxcCompiler.Compile(ShaderStages.Pixel, source, "PSMain");
+
+            ReflectResult result = SpvReflector.Reflect(vs);
+            result = result.Merge(SpvReflector.Reflect(ps));
+
+            using GraphicsContext context = GraphicsContext.Create(RenderBackend);
+
+            context.CreateDevice(true);
+
+            ShaderDesc vsDesc = ShaderDesc.Default(ShaderStages.Vertex, vs, "VSMain");
+            ShaderDesc psDesc = ShaderDesc.Default(ShaderStages.Pixel, ps, "PSMain");
+
+            using Shader vsShader = context.Factory.CreateShader(in vsDesc);
+            using Shader psShader = context.Factory.CreateShader(in psDesc);
+
+            ResourceLayoutDesc layoutDesc1 = ResourceLayoutDesc.Default(result["mvp1"].Desc,
+                                                                        result["mvp2"].Desc);
+            ResourceLayoutDesc layoutDesc2 = ResourceLayoutDesc.Default(result["colors"].Desc,
+                                                                        result["texture"].Desc,
+                                                                        result["texture1"].Desc,
+                                                                        result["texture2", 20].Desc);
+            ResourceLayoutDesc layoutDesc3 = ResourceLayoutDesc.Default(result["samplerState", 4].Desc);
+
+            using ResourceLayout layout1 = context.Factory.CreateResourceLayout(in layoutDesc1);
+            using ResourceLayout layout2 = context.Factory.CreateResourceLayout(in layoutDesc2);
+            using ResourceLayout layout3 = context.Factory.CreateResourceLayout(in layoutDesc3);
+
+            GraphicsShaderDesc shaderDesc = GraphicsShaderDesc.Default(vertex: vsShader, pixel: psShader);
+
+            LayoutDesc layoutDesc = LayoutDesc.Default();
+            layoutDesc.Add(ElementDesc.Default(ElementFormat.Float3, ElementSemanticType.Position));
+            layoutDesc.Add(ElementDesc.Default(ElementFormat.Float4, ElementSemanticType.Color));
+            layoutDesc.Add(ElementDesc.Default(ElementFormat.Float2, ElementSemanticType.TexCoord));
+
+            OutputDesc outputDesc = OutputDesc.Default(TextureSampleCount.Count1,
+                                                       PixelFormat.D24UNormS8UInt,
+                                                       PixelFormat.R8G8B8A8SNorm);
+
+            GraphicsPipelineDesc graphicsPipelineDesc = GraphicsPipelineDesc.Default(shaderDesc,
+                                                                                     [layoutDesc],
+                                                                                     [layout1, layout2, layout3],
+                                                                                     outputDesc);
+
+            using GraphicsPipeline pipeline = context.Factory.CreateGraphicsPipeline(in graphicsPipelineDesc);
+        });
     }
 }
