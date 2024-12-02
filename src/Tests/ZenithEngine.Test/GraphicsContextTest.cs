a18321e3924d33a59fbaa949299425e8576e897e
王先生 <1324771795@qq.com>
2024-12-02 15:16:38 +0800

    重构测试项目并更新相关配置
    删除了 `ZenithEngine.Vulkan.Test` 项目，添加了新的 `ZenithEngine.Test` 项目。
    更新了 `ZenithEngine.sln` 文件，移除了旧项目的配置，添加了新项目的配置。
    在 `GraphicsContextTest.cs` 文件中，移除了旧的测试类，添加了新的测试类，并增加了 `TestShader` 方法。
    在 `MSTestSettings.cs` 文件中，重新添加了 `[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]` 属性。
    删除了旧的 `ZenithEngine.Vulkan.Test.csproj` 项目文件，添加了新的 `ZenithEngine.Test.csproj` 项目文件，并包含了对 `ZenithEngine.ShaderCompiler` 和 `ZenithEngine.Vulkan` 项目的引用。

---
+++ src/Tests/ZenithEngine.Test/GraphicsContextTest.cs
@@ -0,0 +1,33 @@
+﻿using ZenithEngine.Common.Enums;
+using ZenithEngine.Common.Graphics;
+
+namespace ZenithEngine.Test;
+
+[TestClass]
+public class GraphicsContextTest
+{
+    public const Backend RenderBackend = Backend.Vulkan;
+
+    [TestMethod]
+    public void TestCreateDevice()
+    {
+        try
+        {
+            using GraphicsContext context = GraphicsContext.Create(RenderBackend);
+
+            context.CreateDevice(true);
+        }
+        catch (Exception)
+        {
+            Assert.Fail();
+        }
+    }
+
+    [TestMethod]
+    public void TestShader()
+    {
+        using GraphicsContext context = GraphicsContext.Create(RenderBackend);
+
+        context.CreateDevice(true);
+    }
+}
