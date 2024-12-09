305c685f5256099e9e932e7c4644a333c530fea8
王先生 <1324771795@qq.com>
2024-12-09 22:00:45 +0800

    重构窗口接口和事件处理
    重构 `IWindow` 接口，使其继承自 `IWindowEvents` 和 `IWindowProperties` 接口，并移除所有事件和属性的定义。新增 `IWindowEvents` 接口，定义所有窗口事件；新增 `IWindowProperties` 接口，定义所有窗口属性。`Window.Events.cs` 文件中移除 `Window` 类的所有事件定义，`Window.cs` 文件中移除 `Window` 类的所有属性和方法定义。`MouseButtonEventArgs` 类的构造函数增加 `uint clicks` 参数，并添加相应的 `Clicks` 属性。

--- src/ZenithEngine.Windowing/Events/MouseButtonEventArgs.cs
+++ src/ZenithEngine.Windowing/Events/MouseButtonEventArgs.cs
@@ -3,10 +3,12 @@ using ZenithEngine.Windowing.Enums;
 
 namespace ZenithEngine.Windowing.Events;
 
-public class MouseButtonEventArgs(MouseButton button, Vector2D<int> position) : EventArgs
+public class MouseButtonEventArgs(MouseButton button, Vector2D<int> position, uint clicks) : EventArgs
 {
     public MouseButton Button { get; } = button;
 
     public Vector2D<int> Position { get; } = position;
+
+    public uint Clicks { get; } = clicks;
 }
 
--- src/ZenithEngine.Windowing/Interfaces/IWindow.cs
+++ src/ZenithEngine.Windowing/Interfaces/IWindow.cs
@@ -1,76 +1,7 @@
-﻿using Silk.NET.Maths;
-using ZenithEngine.Common.Interfaces;
-using ZenithEngine.Windowing.Enums;
-using ZenithEngine.Windowing.Events;
+﻿namespace ZenithEngine.Windowing.Interfaces;
 
-namespace ZenithEngine.Windowing.Interfaces;
-
-public interface IWindow
+public interface IWindow : IWindowEvents, IWindowProperties
 {
-    event EventHandler<EventArgs>? Loaded;
-
-    event EventHandler<TimeEventArgs>? Update;
-
-    event EventHandler<TimeEventArgs>? Render;
-
-    event EventHandler<ValueEventArgs<WindowState>>? StateChanged;
-
-    event EventHandler<ValueEventArgs<Vector2D<int>>>? PositionChanged;
-
-    event EventHandler<ValueEventArgs<Vector2D<int>>>? SizeChanged;
-
-    event EventHandler<KeyEventArgs>? KeyDown;
-
-    event EventHandler<KeyEventArgs>? KeyUp;
-
-    event EventHandler<ValueEventArgs<char>>? KeyChar;
-
-    event EventHandler<ValueEventArgs<Vector2D<int>>>? MouseMove;
-
-    event EventHandler<MouseButtonEventArgs>? MouseDown;
-
-    event EventHandler<MouseButtonEventArgs>? MouseUp;
-
-    event EventHandler<ValueEventArgs<Vector2D<int>>>? MouseWheel;
-
-    event EventHandler<MouseButtonEventArgs>? Click;
-
-    event EventHandler<MouseButtonEventArgs>? DoubleClick;
-
-    event EventHandler<EventArgs>? Unloaded;
-
-    string Title { get; set; }
-
-    WindowState State { get; set; }
-
-    WindowBorder Border { get; set; }
-
-    bool ShowInTaskbar { get; set; }
-
-    Vector2D<int> Position { get; set; }
-
-    Vector2D<int> Size { get; set; }
-
-    Vector2D<int> MinimumSize { get; set; }
-
-    Vector2D<int> MaximumSize { get; set; }
-
-    double UpdatePerSecond { get; set; }
-
-    double RenderPerSecond { get; set; }
-
-    bool TopMost { get; set; }
-
-    float Opacity { get; set; }
-
-    double Time { get; }
-
-    bool IsFocused { get; }
-
-    float DpiScale { get; }
-
-    ISurface Surface { get; }
-
     void Show();
 
     void Close();
