be712bc1d44b010cbc5646d18e435a307b56d72c
王先生 <1324771795@qq.com>
2024-12-09 18:22:13 +0800

    添加 IWindow 接口定义及相关事件和属性
    在 IWindow.cs 文件中引入多个命名空间：Silk.NET.Maths、ZenithEngine.Common.Interfaces、ZenithEngine.Windowing.Enums 和 ZenithEngine.Windowing.Events。定义了 IWindow 接口，声明了多个事件（如 Loaded、Unloaded、StateChanged 等）和属性（如 Handle、Title、DpiScale 等）。此外，还定义了 Show、Close、Focus 等方法。

---
+++ src/ZenithEngine.Windowing/Interfaces/IWindow.cs
@@ -0,0 +1,89 @@
+﻿using Silk.NET.Maths;
+using ZenithEngine.Common.Interfaces;
+using ZenithEngine.Windowing.Enums;
+using ZenithEngine.Windowing.Events;
+
+namespace ZenithEngine.Windowing.Interfaces;
+
+public interface IWindow
+{
+    event EventHandler<EventArgs>? Loaded;
+
+    event EventHandler<EventArgs>? Unloaded;
+
+    event EventHandler<ValueEventArgs<WindowState>>? StateChanged;
+
+    event EventHandler<ValueEventArgs<Vector2D<int>>>? PositionChanged;
+
+    event EventHandler<ValueEventArgs<Vector2D<int>>>? SizeChanged;
+
+    event EventHandler<KeyEventArgs>? KeyDown;
+
+    event EventHandler<KeyEventArgs>? KeyUp;
+
+    event EventHandler<ValueEventArgs<char>>? KeyChar;
+
+    event EventHandler<ValueEventArgs<Vector2D<int>>>? MouseMove;
+
+    event EventHandler<MouseButtonEventArgs>? MouseDown;
+
+    event EventHandler<MouseButtonEventArgs>? MouseUp;
+
+    event EventHandler<ValueEventArgs<Vector2D<int>>>? MouseWheel;
+
+    event EventHandler<MouseButtonEventArgs>? Click;
+
+    event EventHandler<MouseButtonEventArgs>? DoubleClick;
+
+    event EventHandler<TimeEventArgs>? Update;
+
+    event EventHandler<TimeEventArgs>? Render;
+
+    nint Handle { get; }
+
+    string Title { get; set; }
+
+    float DpiScale { get; }
+
+    WindowState State { get; set; }
+
+    WindowBorder Border { get; set; }
+
+    Vector2D<int> Position { get; set; }
+
+    Vector2D<int> Size { get; set; }
+
+    Vector2D<int> MinimumSize { get; set; }
+
+    Vector2D<int> MaximumSize { get; set; }
+
+    bool IsFocused { get; }
+
+    bool IsVisible { get; set; }
+
+    bool TopMost { get; set; }
+
+    bool ShowInTaskbar { get; set; }
+
+    float Opacity { get; set; }
+
+    double Time { get; }
+
+    double UpdatePerSecond { get; set; }
+
+    double RenderPerSecond { get; set; }
+
+    ISurface Surface { get; }
+
+    void Show();
+
+    void Close();
+
+    void Focus();
+
+    void DoEvents();
+
+    void DoUpdate();
+
+    void DoRender();
+}
