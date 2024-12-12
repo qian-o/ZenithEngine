9d7e6ebf91eb78de377c749648f666f3a1bec112
王先生 <1324771795@qq.com>
2024-12-12 09:52:50 +0800

    修改 WindowController.Loop 方法，增加 autoExit 参数
    在 WindowingTest.cs 文件中：
    * 修改了 WindowController.Loop() 方法的调用，增加了一个布尔参数 true，使其变为 WindowController.Loop(true)。
    
    在 WindowController.cs 文件中：
    * 修改了 Loop 方法的签名，增加了一个布尔参数 autoExit。
    * 在 Loop 方法内部，使用了一个 Func<bool> 类型的 loopFunc 委托，根据 autoExit 参数决定循环条件。如果 autoExit 为 true，则循环条件为 windows.Count > 0，否则为 IsLooping。
    * 增加了一个新的 Exit 方法，该方法将 IsLooping 设为 false。

--- src/Tests/ZenithEngine.Test/WindowingTest.cs
+++ src/Tests/ZenithEngine.Test/WindowingTest.cs
@@ -57,7 +57,7 @@ public class WindowingTest
                 swapChain.Resize();
             };
 
-            WindowController.Loop();
+            WindowController.Loop(true);
         });
     }
 }
