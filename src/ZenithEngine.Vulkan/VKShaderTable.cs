98cf0bca8448f7d39bd492e347c0215114d59ee9
王先生 <1324771795@qq.com>
2025-01-13 18:10:11 +0800

    新增 VKShaderTable 类及相关缓冲区和方法
    添加了多个命名空间引用，包括 `System.Runtime.CompilerServices`、`Silk.NET.Vulkan` 和 `ZenithEngine.Common` 等。新增了 `ZenithEngine.Vulkan` 命名空间，并在该命名空间下新增了一个内部不安全类 `VKShaderTable`，该类继承自 `GraphicsResource`。
    
    在 `VKShaderTable` 类中定义了三个公共字段：`RayGenRegion`、`MissRegion` 和 `HitGroupRegion`，它们的类型均为 `StridedDeviceAddressRegionKHR`。新增了 `VKShaderTable` 类的构造函数，该构造函数接受两个参数：`GraphicsContext context` 和 `VKRayTracingPipeline pipeline`。
    
    在构造函数中，计算了多个与着色器组句柄相关的大小，并分配了内存以存储这些句柄。使用 `Context.KhrRayTracingPipeline.GetRayTracingCaptureReplayShaderGroupHandles` 方法获取着色器组句柄，并将其存储在分配的内存中。创建了三个 `VKBuffer` 实例：`RayGenBuffer`、`MissBuffer` 和 `HitGroupBuffer`，并将着色器组句柄复制到这些缓冲区中。初始化了 `RayGenRegion`、`MissRegion` 和 `HitGroupRegion` 字段，并释放了分配的内存。
    
    定义了一个私有方法 `CopyHandles`，用于将着色器组句柄复制到缓冲区中。新增了三个只读属性：`RayGenBuffer`、`MissBuffer` 和 `HitGroupBuffer`。重写了 `Context` 属性以返回 `VKGraphicsContext` 类型的上下文。重写了 `DebugName` 方法，但该方法目前为空。重写了 `Destroy` 方法，在该方法中释放了 `RayGenBuffer`、`MissBuffer` 和 `HitGroupBuffer`。

---
+++ src/ZenithEngine.Vulkan/VKShaderTable.cs
@@ -0,0 +1,108 @@
+﻿using System.Runtime.CompilerServices;
+using Silk.NET.Vulkan;
+using ZenithEngine.Common;
+using ZenithEngine.Common.Enums;
+using ZenithEngine.Common.Graphics;
+
+namespace ZenithEngine.Vulkan;
+
+internal unsafe class VKShaderTable : GraphicsResource
+{
+    public StridedDeviceAddressRegionKHR RayGenRegion;
+    public StridedDeviceAddressRegionKHR MissRegion;
+    public StridedDeviceAddressRegionKHR HitGroupRegion;
+
+    public VKShaderTable(GraphicsContext context, VKRayTracingPipeline pipeline) : base(context)
+    {
+        uint handleSize = 32;
+        uint handleSizeAligned = Utils.AlignedSize(handleSize, 64u);
+
+        uint missCount = (uint)pipeline.Desc.Shaders.Miss.Length;
+        uint hitGroupCount = (uint)pipeline.Desc.HitGroups.Length;
+        uint groupCount = 1 + missCount + hitGroupCount;
+
+        uint missSize = missCount * handleSize;
+        uint hitGroupSize = hitGroupCount * handleSize;
+        uint groupSize = groupCount * handleSize;
+
+        byte* group = Allocator.Alloc<byte>(groupSize);
+        Context.KhrRayTracingPipeline!.GetRayTracingCaptureReplayShaderGroupHandles(Context.Device,
+                                                                                    pipeline.Pipeline,
+                                                                                    0,
+                                                                                    groupCount,
+                                                                                    groupSize,
+                                                                                    group).ThrowIfError();
+
+        uint missSizeAligned = missCount * handleSizeAligned;
+        uint hitGroupSizeAligned = hitGroupCount * handleSizeAligned;
+
+        RayGenBuffer = new(Context, handleSizeAligned, BufferUsageFlags.ShaderBindingTableBitKhr, true);
+        MissBuffer = new(Context, missSizeAligned, BufferUsageFlags.ShaderBindingTableBitKhr, true);
+        HitGroupBuffer = new(Context, hitGroupSizeAligned, BufferUsageFlags.ShaderBindingTableBitKhr, true);
+
+        CopyHandles(RayGenBuffer, 1);
+        group += handleSize;
+
+        CopyHandles(MissBuffer, missCount);
+        group += missSize;
+
+        CopyHandles(HitGroupBuffer, hitGroupCount);
+        group += hitGroupSize;
+
+        RayGenRegion = new()
+        {
+            DeviceAddress = RayGenBuffer.Address,
+            Stride = handleSizeAligned,
+            Size = handleSizeAligned
+        };
+
+        MissRegion = new()
+        {
+            DeviceAddress = MissBuffer.Address,
+            Stride = handleSizeAligned,
+            Size = missSizeAligned
+        };
+
+        HitGroupRegion = new()
+        {
+            DeviceAddress = HitGroupBuffer.Address,
+            Stride = handleSizeAligned,
+            Size = hitGroupSizeAligned
+        };
+
+        Allocator.Free(group);
+
+        void CopyHandles(VKBuffer buffer, uint count)
+        {
+            MappedResource mapped = Context.MapMemory(RayGenBuffer, MapMode.Write);
+
+            for (uint i = 0; i < count; i++)
+            {
+                Unsafe.CopyBlock((byte*)(mapped.Data + (i * handleSizeAligned)),
+                                 group + (i * handleSize),
+                                 handleSize);
+            }
+
+            Context.UnmapMemory(buffer);
+        }
+    }
+
+    public VKBuffer RayGenBuffer { get; }
+
+    public VKBuffer MissBuffer { get; }
+
+    public VKBuffer HitGroupBuffer { get; }
+
+    private new VKGraphicsContext Context => (VKGraphicsContext)base.Context;
+
+    protected override void DebugName(string name)
+    {
+    }
+
+    protected override void Destroy()
+    {
+        RayGenBuffer.Dispose();
+        MissBuffer.Dispose();
+        HitGroupBuffer.Dispose();
+    }
+}
