72c661c2eb91f0747a65a867f3c1d82927d399a8
王先生 <1324771795@qq.com>
2025-02-02 12:16:30 +0800

    添加 DXDescriptorTableAllocator 类以管理描述符表
    新增 DXDescriptorTableAllocator 类用于管理 DirectX 12 描述符表的分配。引入了 Silk.NET 和 ZenithEngine 命名空间。类中定义了 CPU 和 GPU 描述符堆及相关字段，构造函数初始化这些堆并创建 DescriptorHeapDesc 对象。重载了 HeapType 和 Context 属性，DebugName 方法未实现，Destroy 方法释放资源。

---
+++ src/ZenithEngine.DirectX12/DXDescriptorTableAllocator.cs
@@ -0,0 +1,58 @@
+﻿using Silk.NET.Core.Native;
+using Silk.NET.Direct3D12;
+using ZenithEngine.Common.Graphics;
+
+namespace ZenithEngine.DirectX12;
+
+internal unsafe class DXDescriptorTableAllocator : GraphicsResource
+{
+    public ComPtr<ID3D12DescriptorHeap> CpuHeap;
+    public ComPtr<ID3D12DescriptorHeap> GpuHeap;
+
+    private readonly uint descriptorSize;
+    private readonly CpuDescriptorHandle[] cpuHandles;
+
+    private uint currentOffset;
+    private bool isDirty;
+
+    public DXDescriptorTableAllocator(GraphicsContext context,
+                                      DescriptorHeapType heapType,
+                                      uint numDescriptors) : base(context)
+    {
+        DescriptorHeapDesc cpuDesc = new()
+        {
+            Type = heapType,
+            NumDescriptors = numDescriptors,
+            Flags = DescriptorHeapFlags.None
+        };
+
+        DescriptorHeapDesc gpuDesc = new()
+        {
+            Type = heapType,
+            NumDescriptors = numDescriptors,
+            Flags = DescriptorHeapFlags.ShaderVisible
+        };
+
+        Context.Device.CreateDescriptorHeap(&cpuDesc, out CpuHeap).ThrowIfError();
+        Context.Device.CreateDescriptorHeap(&gpuDesc, out GpuHeap).ThrowIfError();
+
+        descriptorSize = Context.Device.GetDescriptorHandleIncrementSize(heapType);
+        cpuHandles = new CpuDescriptorHandle[numDescriptors];
+
+        HeapType = heapType;
+    }
+
+    public DescriptorHeapType HeapType { get; }
+
+    private new DXGraphicsContext Context => (DXGraphicsContext)base.Context;
+
+    protected override void DebugName(string name)
+    {
+    }
+
+    protected override void Destroy()
+    {
+        GpuHeap.Dispose();
+        CpuHeap.Dispose();
+    }
+}
