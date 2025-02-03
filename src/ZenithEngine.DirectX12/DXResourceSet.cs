7becdf36e01c87165a2d0a017fe15b38259d274f
王先生 <1324771795@qq.com>
2025-02-04 02:32:15 +0800

    增强 DXResourceSet 资源处理逻辑
    在 DXResourceSet.cs 文件中：
    - 添加对 ZenithEngine.Common 及其子命名空间的引用。
    - 修改 GetDescriptorHandles 方法，增加对不同资源类型的处理逻辑。
    - 对 ConstantBuffer 和 StructuredBuffer 类型添加 TODO 注释，需进一步实现动态偏移处理。
    - 新增对 ResourceType.AccelerationStructure 的处理逻辑，标记 TODO 注释。
    - 遇到不支持的资源类型时抛出 ZenithEngineException 异常。

--- src/ZenithEngine.DirectX12/DXResourceSet.cs
+++ src/ZenithEngine.DirectX12/DXResourceSet.cs
@@ -1,5 +1,6 @@
 ﻿using Silk.NET.Core.Native;
 using Silk.NET.Direct3D12;
+using ZenithEngine.Common;
 using ZenithEngine.Common.Descriptions;
 using ZenithEngine.Common.Enums;
 using ZenithEngine.Common.Graphics;
@@ -128,6 +129,87 @@ internal class DXResourceSet : ResourceSet
     private CpuDescriptorHandle[] GetDescriptorHandles(DXResourceBinding[] bindings,
                                                        uint[]? constantBufferOffsets)
     {
-        return new CpuDescriptorHandle[bindings.Sum(static item => item.Indices.Length)];
+        CpuDescriptorHandle[] handles = new CpuDescriptorHandle[bindings.Sum(static item => item.Indices.Length)];
+
+        uint offset = 0;
+        foreach (DXResourceBinding binding in bindings)
+        {
+            foreach (uint index in binding.Indices)
+            {
+                GraphicsResource resource = Desc.Resources[index];
+
+                switch (binding.Type)
+                {
+                    case ResourceType.ConstantBuffer:
+                        {
+                            DXBuffer buffer = (DXBuffer)resource;
+
+                            if (binding.DynamicOffsetIndex is not -1)
+                            {
+                                // TODO: Implement dynamic constant buffer offset.
+                            }
+                            else
+                            {
+                                handles[offset] = buffer.Cbv;
+                            }
+                        }
+                        break;
+                    case ResourceType.StructuredBuffer:
+                        {
+                            DXBuffer buffer = (DXBuffer)resource;
+
+                            if (binding.DynamicOffsetIndex is not -1)
+                            {
+                                // TODO: Implement dynamic structured buffer offset.
+                            }
+                            else
+                            {
+                                handles[offset] = buffer.Srv;
+                            }
+                        }
+                        break;
+                    case ResourceType.StructuredBufferReadWrite:
+                        {
+                            DXBuffer buffer = (DXBuffer)resource;
+
+                            if (binding.DynamicOffsetIndex is not -1)
+                            {
+                                // TODO: Implement dynamic structured buffer offset.
+                            }
+                            else
+                            {
+                                handles[offset] = buffer.Uav;
+                            }
+                        }
+                        break;
+                    case ResourceType.Texture:
+                        {
+                            handles[offset] = ((DXTexture)resource).Srv;
+                        }
+                        break;
+                    case ResourceType.TextureReadWrite:
+                        {
+                            handles[offset] = ((DXTexture)resource).Uav;
+                        }
+                        break;
+                    case ResourceType.Sampler:
+                        {
+                            handles[offset] = ((DXSampler)resource).Handle;
+                        }
+                        break;
+                    case ResourceType.AccelerationStructure:
+                        {
+                            // TODO: Implement acceleration structure.
+                        }
+                        break;
+                    default:
+                        throw new ZenithEngineException(ExceptionHelpers.NotSupported(binding.Type));
+                }
+
+                offset++;
+            }
+        }
+
+        return handles;
     }
 }
