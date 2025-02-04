68b153546cbdcba2ef7d386e86e67aab374a56ee
王先生 <1324771795@qq.com>
2025-02-04 16:55:15 +0800

    添加新的结构体到 ZenithEngine.Common.Graphics
    在 `IndirectDispatchArgs.cs` 文件中添加了新的命名空间 `ZenithEngine.Common.Graphics`，并定义了一个新的结构体 `IndirectDispatchArgs`，包含三个 `uint` 类型的字段：`GroupCountX`、`GroupCountY` 和 `GroupCountZ`。
    
    在 `IndirectDrawArgs.cs` 文件中添加了新的命名空间 `ZenithEngine.Common.Graphics`，并定义了一个新的结构体 `IndirectDrawArgs`，包含四个 `uint` 类型的字段：`VertexCount`、`InstanceCount`、`FirstVertex` 和 `FirstInstance`。
    
    在 `IndirectDrawIndexedArgs.cs` 文件中添加了新的命名空间 `ZenithEngine.Common.Graphics`，并定义了一个新的结构体 `IndirectDrawIndexedArgs`，包含五个 `uint` 类型的字段：`IndexCount`、`InstanceCount`、`FirstIndex`、`VertexOffset` 和 `FirstInstance`。

---
+++ src/ZenithEngine.Common/Graphics/IndirectDrawIndexedArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace ZenithEngine.Common.Graphics;
+
+public struct IndirectDrawIndexedArgs
+{
+    public uint IndexCount;
+
+    public uint InstanceCount;
+
+    public uint FirstIndex;
+
+    public uint VertexOffset;
+
+    public uint FirstInstance;
+}
