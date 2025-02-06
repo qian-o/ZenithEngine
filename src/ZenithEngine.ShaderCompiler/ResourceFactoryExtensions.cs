9096bcd7157ae1051425ef30133a19d955a9ad23
王先生 <1324771795@qq.com>
2025-02-07 00:06:21 +0800

    移除 optimize 参数并始终添加 -O3
    删除了 `optimize` 参数及其相关逻辑。无论是否优化，始终添加 `-O3` 参数。

--- src/ZenithEngine.ShaderCompiler/ResourceFactoryExtensions.cs
+++ src/ZenithEngine.ShaderCompiler/ResourceFactoryExtensions.cs
@@ -11,8 +11,7 @@ public static class ResourceFactoryExtensions
                                        ShaderStages stage,
                                        string source,
                                        string entryPoint,
-                                       Func<string, string>? includeHandler = null,
-                                       bool optimize = false)
+                                       Func<string, string>? includeHandler = null)
     {
         List<string> arguments = [];
 
@@ -23,14 +22,7 @@ public static class ResourceFactoryExtensions
             arguments.Add($"-E {entryPoint}");
         }
 
-        if (optimize)
-        {
-            arguments.Add("-O3");
-        }
-        else
-        {
-            arguments.Add("-Od");
-        }
+        arguments.Add("-O3");
 
         arguments.Add("-Zpr");
 
