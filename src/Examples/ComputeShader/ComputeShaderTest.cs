84e47beaef06c17e802a9c278bd36298b7fe3b4d
王先生 <1324771795@qq.com>
2025-02-08 15:53:32 +0800

    添加 ComputeShader 项目及其相关配置
    在 ZenithEngine.sln 中添加 ComputeShader 项目，并在 GlobalSection(SolutionConfigurationPlatforms) 和 GlobalSection(NestedProjects) 中添加相应配置。
    在 app.manifest 文件中添加 DPI 感知设置。
    在 ComputeShader.csproj 文件中定义项目输出类型为 Exe，引用 app.manifest 文件，并添加对 ..\Common\Common.csproj 的项目引用，配置 Assets 目录下文件的复制行为。
    在 ComputeShaderTest.cs 文件中添加 ComputeShaderTest 类，继承自 VisualTest，并实现 OnLoad、OnUpdate、OnRender、OnSizeChanged 和 OnDestroy 方法。
    在 Program.cs 文件中添加 Program 静态类，在 Main 方法中创建 ComputeShaderTest 实例并调用其 Run 方法。

---
+++ src/Examples/ComputeShader/ComputeShaderTest.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using ZenithEngine.Common.Enums;
+
+namespace ComputeShader;
+
+internal class ComputeShaderTest(Backend backend) : VisualTest("Compute Shader Test", backend)
+{
+    protected override void OnLoad()
+    {
+    }
+
+    protected override void OnUpdate(double deltaTime, double totalTime)
+    {
+    }
+
+    protected override void OnRender(double deltaTime, double totalTime)
+    {
+    }
+
+    protected override void OnSizeChanged(uint width, uint height)
+    {
+    }
+
+    protected override void OnDestroy()
+    {
+    }
+}
