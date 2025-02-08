070295cfd239df7c4bf8559321e48bfc5f600d68
王先生 <1324771795@qq.com>
2025-02-08 23:58:06 +0800

    添加 CornellBox 方法和光线追踪功能
    在 Vertex.cs 文件中：
    - 添加 using ZenithEngine.Common 语句。
    - 在 Vertex 结构体中添加静态方法 CornellBox，根据 materialId 生成顶点和索引数据。
    - 在 CornellBox 方法内部定义辅助方法 AddQuad，用于添加四边形的顶点和索引。
    
    在 Shader.hlsl 文件中：
    - 添加 #define NUM_FOV 0.785398163 和 #define NUM_STEPS 64 宏定义。
    - 添加 RotateY 函数，用于绕 Y 轴旋转三维向量。
    - 添加 OpU 函数，返回两个浮点数向量中较小的一个。
    - 添加 Box 函数，计算点到盒子的距离。
    - 添加 Map 函数，映射位置到距离值。
    - 添加 TraceRay 函数，沿给定方向从一个位置开始进行光线追踪。
    - 修改 RayMarching 函数，增加视场角和纵横比处理，并调用 TraceRay 函数进行光线追踪。

--- src/Examples/ComputeShader/Assets/Shaders/Shader.hlsl
+++ src/Examples/ComputeShader/Assets/Shaders/Shader.hlsl
@@ -1,6 +1,8 @@
 ﻿#define NUM_AA 8
+#define NUM_FOV 0.785398163
 #define NUM_MIN 0.1
 #define NUM_MAX 100
+#define NUM_STEPS 64
 
 struct Constants
 {
@@ -14,12 +16,95 @@ struct Constants
 ConstantBuffer<Constants> constants : register(b0, space0);
 RWTexture2D<float4> Output : register(u0, space0);
 
+float3 RotateY(float3 p, float a)
+{
+    float c = cos(a);
+    float s = sin(a);
+
+    return float3(p.x * c + p.z * s, p.y, -p.x * s + p.z * c);
+}
+
+float2 OpU(float2 d1, float2 d2)
+{
+    return (d1.x < d2.x) ? d1 : d2;
+}
+
+float Box(float3 p, float3 b)
+{
+    float3 d = abs(p) - b;
+
+    return min(max(d.x, max(d.y, d.z)), 0.0) + length(max(d, 0.0));
+}
+
+float Map(float3 position)
+{
+    float d = 1.0;
+    float3 p = position;
+
+    p = RotateY(p, constants.TotalTime);
+    d = OpU(d, float2(Box(p, float3(0.5, 0.5, 0.5)), 1));
+
+    return d;
+}
+
+float2 TraceRay(float3 position, float3 direction)
+{
+    float min = NUM_MIN;
+    float max = NUM_MAX;
+
+    float2 result = float2(-1, -1);
+
+    for (uint i = 0; i < NUM_STEPS; i++)
+    {
+        if (position.z > max)
+        {
+            break;
+        }
+
+        float hit = Map(position);
+        
+        if (hit < 0.01)
+        {
+            result = hit;
+
+            break;
+        }
+
+        position += direction * hit.x;
+    }
+
+    return result;
+}
+
 float3 RayMarching(float2 xy)
 {
     float2 uv = xy / constants.Resolution;
     uv = uv * 2.0 - 1.0;
 
-    return float3(uv, 0);
+    float aspect = constants.Resolution.x / constants.Resolution.y;
+
+    if (aspect > 1.0)
+    {
+        uv.x *= aspect * NUM_FOV;
+        uv.y *= NUM_FOV;
+    }
+    else
+    {
+        uv.x *= NUM_FOV;
+        uv.y *= NUM_FOV / aspect;
+    }
+
+    float3 position = float3(0, 0, -10);
+    float3 direction = normalize(float3(uv, 1));
+
+    float2 result = TraceRay(position, direction);
+
+    if (result.y < 0.0)
+    {
+        return float3(0, 0, 0);
+    }
+
+    return direction;
 }
 
 [numthreads(8, 8, 1)]
