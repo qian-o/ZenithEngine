12f2741ed365e8afe4b60c2a14e881fdb314929e
王先生 <1324771795@qq.com>
2025-02-08 18:16:01 +0800

    添加抗锯齿逻辑并优化Shader代码
    在代码中添加了宏定义 `NUM_AA` 并将其值设为 8。
    在 `Constants` 结构体定义之前添加了 `#define NUM_AA 8`。
    在 `Shader.hlsl` 文件中添加了 `RayMarching` 函数。
    在 `CSMain` 函数中，移除了计算 `uv` 的代码，并根据 `NUM_AA` 的值添加了抗锯齿处理逻辑。
    如果 `NUM_AA` 等于 1，则直接调用 `RayMarching` 函数并将结果写入 `Output` 纹理。
    如果 `NUM_AA` 大于 1，则在 `xy` 坐标的基础上进行多次采样，并将采样结果累加后取平均值，再写入 `Output` 纹理。
    移除了原来的 `Output` 赋值语句，并根据新的抗锯齿逻辑添加了新的 `Output` 赋值语句。

--- src/Examples/ComputeShader/Assets/Shaders/Shader.hlsl
+++ src/Examples/ComputeShader/Assets/Shaders/Shader.hlsl
@@ -1,4 +1,6 @@
-﻿struct Constants
+﻿#define NUM_AA 8
+
+struct Constants
 {
     uint2 Resolution;
 
@@ -10,10 +12,39 @@
 ConstantBuffer<Constants> constants : register(b0, space0);
 RWTexture2D<float4> Output : register(u0, space0);
 
+float3 RayMarching(float2 xy)
+{
+    float2 uv = xy / float2(constants.Resolution);
+    uv = uv * 2.0 - 1.0;
+
+    return float3(uv, 0);
+}
+
 [numthreads(8, 8, 1)]
 void CSMain(uint3 id : SV_DispatchThreadID)
 {
-    float2 uv = float2(id.xy) / float2(constants.Resolution);
+    if (NUM_AA == 1)
+    {
+        Output[id.xy] = float4(RayMarching(float2(id.xy)), 1);
+    }
+    else
+    {
+        float2 xy = float2(id.xy);
 
-    Output[id.xy] = float4(uv, 0, 1);
+        const float halfAA = float(NUM_AA) * 0.5;
+
+        float3 color = 0;
+        for (uint i = 0; i < NUM_AA; i++)
+        {
+            for (uint j = 0; j < NUM_AA; j++)
+            {
+                float2 offset = float2(i, j) / halfAA;
+
+                color += RayMarching(xy + offset);
+            }
+        }
+        color /= float(NUM_AA * NUM_AA);
+
+        Output[id.xy] = float4(color, 1);
+    }
 }
