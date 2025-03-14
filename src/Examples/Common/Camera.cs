a39b2cdbda5fe7c1421e528e312b03025f0307da
王先生 <1324771795@qq.com>
2025-03-14 10:09:03 +0800

    重构项目结构并更新输入控制器接口
    在 `ZenithEngine.sln` 中重新组织项目结构，便于管理和查找。更新 `IInputController` 接口，修改事件类型并添加新事件以支持更多输入。新增 `Camera` 类，处理窗口鼠标事件并定义相机基本属性。更新 `ZenithEngine.slnx` 文件以增强可读性。

---
+++ src/Examples/Common/Camera.cs
@@ -0,0 +1,43 @@
+﻿using Silk.NET.Maths;
+using ZenithEngine.Common.Events;
+using ZenithEngine.Windowing.Interfaces;
+
+namespace Common;
+
+public class Camera
+{
+    public Camera(IWindow window)
+    {
+        window.MouseDown += Window_MouseDown;
+        window.MouseMove += Window_MouseMove;
+        window.MouseUp += Window_MouseUp;
+    }
+
+    public Vector3D<float> Position { get; private set; } = Vector3D<float>.Zero;
+
+    public Vector3D<float> Front { get; private set; } = Vector3D<float>.UnitZ;
+
+    public Vector3D<float> Up { get; private set; } = Vector3D<float>.UnitY;
+
+    public Vector3D<float> Right { get; private set; } = Vector3D<float>.UnitX;
+
+    public float NearPlane { get; private set; } = 0.1f;
+
+    public float FarPlane { get; private set; } = 1000.0f;
+
+    public float Fov { get; private set; } = 45.0f;
+
+    public float AspectRatio { get; private set; } = 1.0f;
+
+    private void Window_MouseDown(object? sender, MouseButtonEventArgs e)
+    {
+    }
+
+    private void Window_MouseMove(object? sender, ValueEventArgs<Vector2D<int>> e)
+    {
+    }
+
+    private void Window_MouseUp(object? sender, MouseButtonEventArgs e)
+    {
+    }
+}
