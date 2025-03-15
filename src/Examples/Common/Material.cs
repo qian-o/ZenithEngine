3077a8e95c8b814f3dd4c1d7c4dd669d75d288c7
王先生 <1324771795@qq.com>
2025-03-15 23:59:19 +0800

    增强 Material 结构体的内存布局
    在 `Material.cs` 文件中，添加了 `System.Runtime.InteropServices` 的引用，并在 `Material` 结构体上添加了 `StructLayout` 特性以指定内存布局。该结构体现在包含三个字段：`IsLight`（布尔值），`Albedo`（表示颜色的三维向量），以及 `Emission`（表示发光的三维向量），并通过 `FieldOffset` 特性指定了它们在内存中的偏移量。

--- src/Examples/Common/Material.cs
+++ src/Examples/Common/Material.cs
@@ -1,12 +1,17 @@
-﻿using Silk.NET.Maths;
+﻿using System.Runtime.InteropServices;
+using Silk.NET.Maths;
 
 namespace Common;
 
+[StructLayout(LayoutKind.Explicit)]
 public struct Material
 {
+    [FieldOffset(0)]
     public bool IsLight;
 
+    [FieldOffset(4)]
     public Vector3D<float> Albedo;
 
+    [FieldOffset(16)]
     public Vector3D<float> Emission;
 }
