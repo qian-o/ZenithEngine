098608bb62879360ebaf0b793abfa1f28feb9e58
王先生 <1324771795@qq.com>
2025-05-11 03:24:49 +0800

    优化 ShaderReflection 以增强类型安全性
    在 `ShaderReflection.cs` 中添加了对 `System.Collections.ObjectModel` 的引用，并将 `ShaderBinding` 的 `type` 属性更改为 `Type`。将 `bindings` 字典更改为 `ReadOnlyDictionary` 以提高安全性。构造函数中初始化 `bindings`，并添加了对 `binding.Used` 的检查。新增 `ParseParameterBlock` 方法以递归解析参数块。修改 `GetResourceType` 方法以支持 `DescriptorTableSlot`，并重构资源类型处理逻辑。删除 `ResourceCategory` 方法，使用 `Indistinct` 进行资源类型判断。最后，在 `Resource` 方法中添加了对资源访问权限的检查。

--- src/ZenithEngine.ShaderCompiler/ShaderReflection.cs
+++ src/ZenithEngine.ShaderCompiler/ShaderReflection.cs
@@ -1,4 +1,5 @@
-﻿using Slangc.NET;
+﻿using System.Collections.ObjectModel;
+using Slangc.NET;
 using Slangc.NET.Enums;
 using Slangc.NET.Models;
 using ZenithEngine.Common;
@@ -8,31 +9,38 @@ namespace ZenithEngine.ShaderCompiler;
 
 public class ShaderReflection
 {
-    private record ShaderBinding(string Name, ShaderStages Stages, ResourceType type, uint Space, uint Index, uint Count);
+    private record ShaderBinding(string Name, ShaderStages Stages, ResourceType Type, uint Space, uint Index, uint Count);
 
-    private readonly Dictionary<string, ShaderBinding> bindings = [];
+    private readonly ReadOnlyDictionary<string, ShaderBinding> cache;
 
     internal ShaderReflection(ShaderStages stage, SlangReflection slangReflection)
     {
         SlangEntryPoint entryPoint = slangReflection.EntryPoints[0];
 
+        Dictionary<string, ShaderBinding> bindings = [];
+
         foreach (SlangNamedTypeBinding namedTypeBinding in entryPoint.Bindings)
         {
             SlangBinding binding = namedTypeBinding.Binding;
 
-            //if (!binding.Used)
-            //{
-            //    continue;
-            //}
+            if (!binding.Used)
+            {
+                continue;
+            }
+
+            SlangParameter parameter = slangReflection.Parameters.First(item => item.Name == namedTypeBinding.Name);
 
             if (binding.Kind is SlangParameterCategory.SubElementRegisterSpace)
             {
-                // 递归结构体。
+                if (parameter.Type.Kind is not SlangTypeKind.ParameterBlock)
+                {
+                    throw new ZenithEngineException(ExceptionHelpers.NotSupported(parameter.Type.Kind));
+                }
+
+                ParseParameterBlock(namedTypeBinding.Name, stage, bindings, parameter.Type);
             }
             else
             {
-                SlangParameter parameter = slangReflection.Parameters.First(item => item.Name == namedTypeBinding.Name);
-
                 bindings.Add(namedTypeBinding.Name, new(namedTypeBinding.Name,
                                                         stage,
                                                         GetResourceType(binding.Kind, parameter.Type),
@@ -41,15 +49,64 @@ public class ShaderReflection
                                                         binding.Count));
             }
         }
+
+        cache = new(bindings);
     }
 
     internal ShaderReflection(ShaderReflection[] reflections)
     {
+        Dictionary<string, ShaderBinding> bindings = [];
+
+        foreach (ShaderReflection reflection in reflections)
+        {
+            foreach (KeyValuePair<string, ShaderBinding> binding in reflection.cache)
+            {
+                if (!bindings.TryGetValue(binding.Key, out ShaderBinding? value))
+                {
+                    bindings.Add(binding.Key, binding.Value);
+                }
+                else
+                {
+                    ShaderStages stages = value.Stages | binding.Value.Stages;
+
+                    bindings[binding.Key] = new(binding.Value.Name,
+                                                stages,
+                                                binding.Value.Type,
+                                                binding.Value.Space,
+                                                binding.Value.Index,
+                                                binding.Value.Count);
+                }
+            }
+        }
+
+        cache = new(bindings);
     }
 
     public ShaderReflection Merge(ShaderReflection other)
     {
         return new ShaderReflection([this, other]);
+    }
+
+    private static void ParseParameterBlock(string name, ShaderStages stage, Dictionary<string, ShaderBinding> bindings, SlangType type)
+    {
+        foreach (SlangVar var in type.ParameterBlock!.ElementType.Struct!.Fields)
+        {
+            string varName = $"{name}.{var.Name}";
+
+            if (var.Type.Kind is SlangTypeKind.ParameterBlock)
+            {
+                ParseParameterBlock(varName, stage, bindings, var.Type);
+            }
+            else
+            {
+                bindings.Add(varName, new(varName,
+                                          stage,
+                                          GetResourceType(var.Binding!.Kind, var.Type),
+                                          var.Binding.Space,
+                                          var.Binding.Index,
+                                          var.Binding.Count));
+            }
+        }
     }
 
     private static ResourceType GetResourceType(SlangParameterCategory parameterCategory, SlangType type)
@@ -61,24 +118,28 @@ public class ShaderReflection
 
             SlangParameterCategory.SamplerState => ResourceType.Sampler,
 
-            SlangParameterCategory.ShaderResource => ResourceCategory(type, false),
-
-            SlangParameterCategory.UnorderedAccess => ResourceCategory(type, true),
+            SlangParameterCategory.ShaderResource or
+            SlangParameterCategory.UnorderedAccess or
+            SlangParameterCategory.DescriptorTableSlot => Indistinct(type),
 
             _ => throw new ZenithEngineException(ExceptionHelpers.NotSupported(parameterCategory))
         };
 
-        static ResourceType ResourceCategory(SlangType type, bool isReadWrite)
+        static ResourceType Indistinct(SlangType type)
         {
-            if (type.Kind is SlangTypeKind.Array)
+            return type.Kind switch
             {
-                return ResourceCategory(type.Array!.ElementType, isReadWrite);
-            }
+                SlangTypeKind.Array => Indistinct(type.Array!.ElementType),
+                SlangTypeKind.ConstantBuffer => ResourceType.ConstantBuffer,
+                SlangTypeKind.Resource => Resource(type),
+                SlangTypeKind.SamplerState => ResourceType.Sampler,
+                _ => throw new ZenithEngineException(ExceptionHelpers.NotSupported(type.Kind))
+            };
+        }
 
-            if (type.Kind is not SlangTypeKind.Resource)
-            {
-                throw new ZenithEngineException(ExceptionHelpers.NotSupported(type.Kind));
-            }
+        static ResourceType Resource(SlangType type)
+        {
+            bool isReadWrite = type.Resource!.Access is SlangResourceAccess.ReadWrite;
 
             return type.Resource!.BaseShape switch
             {
@@ -91,7 +152,7 @@ public class ShaderReflection
 
                 SlangResourceShape.AccelerationStructure => ResourceType.AccelerationStructure,
 
-                _ => throw new ZenithEngineException(ExceptionHelpers.NotSupported(type.Resource.BaseShape)),
+                _ => throw new ZenithEngineException(ExceptionHelpers.NotSupported(type.Resource.BaseShape))
             };
         }
     }
