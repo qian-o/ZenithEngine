600c7fa7bf95e84d288da7d24942683f0013840b
王先生 <1324771795@qq.com>
2025-05-20 23:25:32 +0800

    优化着色器代码和全局变量使用
    在 `ClosestHit.slang` 中，使用 `SampleLevel` 替代 `Sample` 方法以提高纹理采样精度。
    在 `HDR.slang` 中，简化函数签名，使用全局变量 `hdrTextures` 和 `globals.HdrResolution`。
    在 `Lighting.slang` 中，移除 `useHdr` 参数，改为使用 `globals.UseHdr`。
    在 `Sampling.slang` 中，简化 `InteresetsEmitter` 函数参数，使用 `globals.LightCount`。
    在 `Structs.slang` 中，修改 `Payload` 结构体的 `Beta` 字段初始化方式。
    在 `Uniforms.slang` 中，调整 `samplers` 的空间分配。
    在 `RayTracingTest.cs` 中，更新着色器编译路径以确保正确加载 `ClosestHit.slang`。

--- src/Examples/RayTracing/RayTracingTest.cs
+++ src/Examples/RayTracing/RayTracingTest.cs
@@ -1,4 +1,6 @@
 ﻿using Common;
+using ZenithEngine.Common.Enums;
+using ZenithEngine.ShaderCompiler;
 
 namespace RayTracing;
 
@@ -6,6 +8,9 @@ internal unsafe class RayTracingTest() : VisualTest("RayTracing Test")
 {
     protected override void OnLoad()
     {
+        string ch = Path.Combine(AppContext.BaseDirectory, "Assets", "Shaders", "ClosestHit.slang");
+
+        Context.Factory.CompileShader(ch, ShaderStages.ClosestHit, "Main", out ShaderReflection chReflection);
     }
 
     protected override void OnUpdate(double deltaTime, double totalTime)
