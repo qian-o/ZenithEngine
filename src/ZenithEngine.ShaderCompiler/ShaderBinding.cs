bf650b3bf31383b9a653eedf5b6562b5e41d3adb
王先生 <1324771795@qq.com>
2025-05-21 09:42:05 +0800

    增强 ShaderBinding 的可读性
    在 `ShaderBinding.cs` 文件中，添加了 `ToString` 方法，返回包含 `Space` 和 `Desc` 详细信息的格式化字符串，提升了结构体的可读性和调试能力。

--- src/ZenithEngine.ShaderCompiler/ShaderBinding.cs
+++ src/ZenithEngine.ShaderCompiler/ShaderBinding.cs
@@ -7,4 +7,9 @@ public readonly struct ShaderBinding(uint space, ResourceElementDesc desc)
     public uint Space { get; } = space;
 
     public ResourceElementDesc Desc { get; } = desc;
+
+    public override string ToString()
+    {
+        return $"Space: {Space}, Index: {Desc.Index}, Type: {Desc.Type}";
+    }
 }
