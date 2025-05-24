674e46096452af3b2df7a5d980feee3906854eca
王先生 <1324771795@qq.com>
2025-05-24 16:35:07 +0800

    增强材质和顶点结构功能
    在 `Material.cs` 中更新 `Material` 结构，修改字段类型为 `Vector4D<float>`，添加新字段并移除 `IsLight`。在 `Vertex.cs` 中添加 `materialIndex` 参数，更新 `GetInputLayout` 和 `AddQuad` 方法以支持新结构。更新 `material` 初始化逻辑，调整 `TriangleTest.cs` 中的 `Vertex` 数组以包含 `materialIndex`。这些更改旨在支持更多材质属性和更灵活的顶点数据结构。

--- src/Examples/Common/Vertex.cs
+++ src/Examples/Common/Vertex.cs
@@ -5,13 +5,15 @@ using ZenithEngine.Common.Enums;
 
 namespace Common;
 
-public struct Vertex(Vector3D<float> position, Vector3D<float> normal, Vector2D<float> texCoord)
+public struct Vertex(Vector3D<float> position, Vector3D<float> normal, Vector2D<float> texCoord, int materialIndex)
 {
     public Vector3D<float> Position = position;
 
     public Vector3D<float> Normal = normal;
 
     public Vector2D<float> TexCoord = texCoord;
+
+    public int MaterialIndex = materialIndex;
 
     public static InputLayoutDesc GetInputLayout()
     {
@@ -20,6 +22,7 @@ public struct Vertex(Vector3D<float> position, Vector3D<float> normal, Vector2D<
         inputLayout.Add(new(ElementFormat.Float3, ElementSemanticType.Position, 0));
         inputLayout.Add(new(ElementFormat.Float3, ElementSemanticType.Normal, 0));
         inputLayout.Add(new(ElementFormat.Float2, ElementSemanticType.TexCoord, 0));
+        inputLayout.Add(new(ElementFormat.Int1, ElementSemanticType.BlendIndices, 0));
 
         return inputLayout;
     }
@@ -43,84 +46,100 @@ public struct Vertex(Vector3D<float> position, Vector3D<float> normal, Vector2D<
                 AddQuad(new(552.8f, 0.0f, 0.0f),
                         new(549.6f, 0.0f, 559.2f),
                         new(556.0f, 548.8f, 559.2f),
-                        new(556.0f, 548.8f, 0.0f));
+                        new(556.0f, 548.8f, 0.0f),
+                        (int)materialId);
                 break;
             case 1:
                 // Right wall (green)
                 AddQuad(new(0.0f, 0.0f, 559.2f),
                         new(0.0f, 0.0f, 0.0f),
                         new(0.0f, 548.8f, 0.0f),
-                        new(0.0f, 548.8f, 559.2f));
+                        new(0.0f, 548.8f, 559.2f),
+                        (int)materialId);
                 break;
             case 2:
                 // Ceiling (white)
                 AddQuad(new(556.0f, 548.8f, 0.0f),
                         new(556.0f, 548.8f, 559.2f),
                         new(0.0f, 548.8f, 559.2f),
-                        new(0.0f, 548.8f, 0.0f));
+                        new(0.0f, 548.8f, 0.0f),
+                        (int)materialId);
 
                 // Floor (white)
                 AddQuad(new(552.8f, 0.0f, 0.0f),
                         new(0.0f, 0.0f, 0.0f),
                         new(0.0f, 0.0f, 559.2f),
-                        new(549.6f, 0.0f, 559.2f));
+                        new(549.6f, 0.0f, 559.2f),
+                        (int)materialId);
 
                 // Back wall (white)
                 AddQuad(new(549.6f, 0.0f, 559.2f),
                         new(0.0f, 0.0f, 559.2f),
                         new(0.0f, 548.8f, 559.2f),
-                        new(556.0f, 548.8f, 559.2f));
+                        new(556.0f, 548.8f, 559.2f),
+                        (int)materialId);
 
                 // Short block (white)
                 AddQuad(new(130.0f, 165.0f, 65.0f),
                         new(82.0f, 165.0f, 225.0f),
                         new(240.0f, 165.0f, 272.0f),
-                        new(290.0f, 165.0f, 114.0f));
+                        new(290.0f, 165.0f, 114.0f),
+                        (int)materialId);
                 AddQuad(new(290.0f, 0.0f, 114.0f),
                         new(290.0f, 165.0f, 114.0f),
                         new(240.0f, 165.0f, 272.0f),
-                        new(240.0f, 0.0f, 272.0f));
+                        new(240.0f, 0.0f, 272.0f),
+                        (int)materialId);
                 AddQuad(new(130.0f, 0.0f, 65.0f),
                         new(130.0f, 165.0f, 65.0f),
                         new(290.0f, 165.0f, 114.0f),
-                        new(290.0f, 0.0f, 114.0f));
+                        new(290.0f, 0.0f, 114.0f),
+                        (int)materialId);
                 AddQuad(new(82.0f, 0.0f, 225.0f),
                         new(82.0f, 165.0f, 225.0f),
                         new(130.0f, 165.0f, 65.0f),
-                        new(130.0f, 0.0f, 65.0f));
+                        new(130.0f, 0.0f, 65.0f),
+                        (int)materialId);
                 AddQuad(new(240.0f, 0.0f, 272.0f),
                         new(240.0f, 165.0f, 272.0f),
                         new(82.0f, 165.0f, 225.0f),
-                        new(82.0f, 0.0f, 225.0f));
+                        new(82.0f, 0.0f, 225.0f),
+                        (int)materialId);
 
                 // Tall block (white)
                 AddQuad(new(423.0f, 330.0f, 247.0f),
                         new(265.0f, 330.0f, 296.0f),
                         new(314.0f, 330.0f, 456.0f),
-                        new(472.0f, 330.0f, 406.0f));
+                        new(472.0f, 330.0f, 406.0f),
+                        (int)materialId);
                 AddQuad(new(423.0f, 0.0f, 247.0f),
                         new(423.0f, 330.0f, 247.0f),
                         new(472.0f, 330.0f, 406.0f),
-                        new(472.0f, 0.0f, 406.0f));
+                        new(472.0f, 0.0f, 406.0f),
+                        (int)materialId);
                 AddQuad(new(472.0f, 0.0f, 406.0f),
                         new(472.0f, 330.0f, 406.0f),
                         new(314.0f, 330.0f, 456.0f),
-                        new(314.0f, 0.0f, 456.0f));
+                        new(314.0f, 0.0f, 456.0f),
+                        (int)materialId);
                 AddQuad(new(314.0f, 0.0f, 456.0f),
                         new(314.0f, 330.0f, 456.0f),
                         new(265.0f, 330.0f, 296.0f),
-                        new(265.0f, 0.0f, 296.0f));
+                        new(265.0f, 0.0f, 296.0f),
+                        (int)materialId);
                 AddQuad(new(265.0f, 0.0f, 296.0f),
                         new(265.0f, 330.0f, 296.0f),
                         new(423.0f, 330.0f, 247.0f),
-                        new(423.0f, 0.0f, 247.0f));
+                        new(423.0f, 0.0f, 247.0f),
+                        (int)materialId);
                 break;
             case 3:
                 // Light (short block)
                 AddQuad(new(343.0f, 548.6f, 227.0f),
                         new(343.0f, 548.6f, 332.0f),
                         new(213.0f, 548.6f, 332.0f),
-                        new(213.0f, 548.6f, 227.0f));
+                        new(213.0f, 548.6f, 227.0f),
+                        (int)materialId);
                 break;
             default:
                 throw new ZenithEngineException(ExceptionHelpers.NotSupported(materialId));
@@ -132,27 +151,87 @@ public struct Vertex(Vector3D<float> position, Vector3D<float> normal, Vector2D<
         {
             0 => new()
             {
-                IsLight = false,
-                Albedo = new(0.63f, 0.065f, 0.05f),
-                Emission = new(0.0f)
+                Albedo = new(0.63f, 0.06f, 0.06f, 0.50f),
+                Emission = new(0.0f),
+                Extinction = new(1.0f, 1.0f, 1.0f, 0.0f),
+                Metallic = 0.0f,
+                Roughness = 0.5f,
+                SubSurface = 0.0f,
+                SpecularTint = 0.0f,
+                Sheen = 0.0f,
+                SheenTint = 0.0f,
+                ClearCoat = 0.0f,
+                ClearCoatGloss = 0.0f,
+                Transmission = 0.0f,
+                IOR = 1.45f,
+                AttenuationDistance = 1.0f,
+                AlbedoTextureIndex = -1,
+                MetallicRoughnessTextureIndex = -1,
+                NormalTextureIndex = -1,
+                HeightTextureIndex = -1
             },
             1 => new()
             {
-                IsLight = false,
-                Albedo = new(0.14f, 0.45f, 0.091f),
-                Emission = new(0.0f)
+                Albedo = new(0.14f, 0.45f, 0.09f, 0.50f),
+                Emission = new(0.0f),
+                Extinction = new(1.0f, 1.0f, 1.0f, 0.0f),
+                Metallic = 0.0f,
+                Roughness = 0.5f,
+                SubSurface = 0.0f,
+                SpecularTint = 0.0f,
+                Sheen = 0.0f,
+                SheenTint = 0.0f,
+                ClearCoat = 0.0f,
+                ClearCoatGloss = 0.0f,
+                Transmission = 0.0f,
+                IOR = 1.45f,
+                AttenuationDistance = 1.0f,
+                AlbedoTextureIndex = -1,
+                MetallicRoughnessTextureIndex = -1,
+                NormalTextureIndex = -1,
+                HeightTextureIndex = -1
             },
             2 => new()
             {
-                IsLight = false,
-                Albedo = new(0.725f, 0.71f, 0.68f),
-                Emission = new(0.0f)
+                Albedo = new(0.73f, 0.71f, 0.68f, 0.50f),
+                Emission = new(0.0f),
+                Extinction = new(1.0f, 1.0f, 1.0f, 0.0f),
+                Metallic = 0.0f,
+                Roughness = 0.5f,
+                SubSurface = 0.0f,
+                SpecularTint = 0.0f,
+                Sheen = 0.0f,
+                SheenTint = 0.0f,
+                ClearCoat = 0.0f,
+                ClearCoatGloss = 0.0f,
+                Transmission = 0.0f,
+                IOR = 1.45f,
+                AttenuationDistance = 1.0f,
+                AlbedoTextureIndex = -1,
+                MetallicRoughnessTextureIndex = -1,
+                NormalTextureIndex = -1,
+                HeightTextureIndex = -1
             },
             3 => new()
             {
-                IsLight = true,
-                Albedo = new(0.65f),
-                Emission = new(47.8348007f, 38.5663986f, 31.0807991f)
+                Albedo = new(1.0f, 1.0f, 1.0f, 0.50f),
+                Emission = new(0.0f),
+                Extinction = new(1.0f, 1.0f, 1.0f, 0.0f),
+                Metallic = 0.0f,
+                Roughness = 0.5f,
+                SubSurface = 0.0f,
+                SpecularTint = 0.0f,
+                Sheen = 0.0f,
+                SheenTint = 0.0f,
+                ClearCoat = 0.0f,
+                ClearCoatGloss = 0.0f,
+                Transmission = 0.0f,
+                IOR = 1.45f,
+                AttenuationDistance = 1.0f,
+                AlbedoTextureIndex = -1,
+                MetallicRoughnessTextureIndex = -1,
+                NormalTextureIndex = -1,
+                HeightTextureIndex = -1
             },
             _ => throw new ZenithEngineException(ExceptionHelpers.NotSupported(materialId))
         };
@@ -160,16 +239,17 @@ public struct Vertex(Vector3D<float> position, Vector3D<float> normal, Vector2D<
         void AddQuad(Vector3D<float> v0,
                      Vector3D<float> v1,
                      Vector3D<float> v2,
-                     Vector3D<float> v3)
+                     Vector3D<float> v3,
+                     int materialIndex)
         {
             Vector3D<float> normal = Vector3D.Normalize(Vector3D.Cross(v1 - v0, v2 - v0));
 
             uint startIndex = (uint)verticesList.Count;
 
-            verticesList.Add(new(v0, normal, new(0, 0)));
-            verticesList.Add(new(v1, normal, new(1, 0)));
-            verticesList.Add(new(v2, normal, new(1, 1)));
-            verticesList.Add(new(v3, normal, new(0, 1)));
+            verticesList.Add(new(v0, normal, new(0, 0), materialIndex));
+            verticesList.Add(new(v1, normal, new(1, 0), materialIndex));
+            verticesList.Add(new(v2, normal, new(1, 1), materialIndex));
+            verticesList.Add(new(v3, normal, new(0, 1), materialIndex));
 
             indicesList.Add(startIndex);
             indicesList.Add(startIndex + 1);
